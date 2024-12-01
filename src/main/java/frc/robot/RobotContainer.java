d3e65374baf88dc85b6d0bf9a5dc2fc02c733abe
Joshua Stoddard <joshua.azro@gmail.com>
2024-11-30 22:51:30 -0500

    Made some changes based off of review

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,30 +9,25 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class RobotContainer {
-    private double MaxSpeed =
-            TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    private double MaxAngularRate =
-            RotationsPerSecond.of(0.75)
-                    .in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
+    private static double MaxSpeed = DrivetrainConstants.MaxSpeed;
+    private static double MaxAngularRate = DrivetrainConstants.MaxAngularRate;
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive =
             new SwerveRequest.FieldCentric()
                     .withDeadband(MaxSpeed * 0.1)
                     .withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
                     .withDriveRequestType(
-                            DriveRequestType
-                                    .OpenLoopVoltage); // Use open-loop control for drive motors
+                            DriveRequestType.Velocity); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
     private final SwerveRequest.RobotCentric forwardStraight =
@@ -63,47 +58,18 @@ public class RobotContainer {
                         () ->
                                 drive.withVelocityX(
                                                 -joystick.getLeftY()
-                                                        * MaxSpeed) // Drive forward with negative Y
+                                                        * MaxSpeed) // Drive forward with
+                                        // negative Y
                                         // (forward)
                                         .withVelocityY(
-                                                -joystick.getLeftX()
-                                                        * MaxSpeed) // Drive left with negative X
+                                                -joystick.getLeftX() * MaxSpeed) // Drive left with
+                                        // negative X
                                         // (left)
                                         .withRotationalRate(
-                                                -joystick.getRightX()
-                                                        * MaxAngularRate) // Drive counterclockwise
+                                                -joystick.getRightX() * MaxAngularRate) // Drive
+                        // counterclockwise
                         // with negative X (left)
                         ));
-
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b()
-                .whileTrue(
-                        drivetrain.applyRequest(
-                                () ->
-                                        point.withModuleDirection(
-                                                new Rotation2d(
-                                                        -joystick.getLeftY(),
-                                                        -joystick.getLeftX()))));
-
-        joystick.pov(0)
-                .whileTrue(
-                        drivetrain.applyRequest(
-                                () -> forwardStraight.withVelocityX(0.5).withVelocityY(0)));
-        joystick.pov(180)
-                .whileTrue(
-                        drivetrain.applyRequest(
-                                () -> forwardStraight.withVelocityX(-0.5).withVelocityY(0)));
-
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start()
-                .and(joystick.y())
-                .whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start()
-                .and(joystick.x())
-                .whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // reset the field-centric heading on left bumper press
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
