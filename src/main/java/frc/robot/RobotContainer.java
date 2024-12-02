ac8147ae2625f33b4a3e67ae357c681c63b1f072
Joshua Stoddard <joshua.azro@gmail.com>
2024-12-02 17:39:43 -0500

    Added 2 Joysticks for drivetrain

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,10 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
@@ -28,14 +28,11 @@ public class RobotContainer {
                     .withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
                     .withDriveRequestType(
                             DriveRequestType.Velocity); // Use open-loop control for drive motors
-    private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-    private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
-    private final SwerveRequest.RobotCentric forwardStraight =
-            new SwerveRequest.RobotCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    private final CommandXboxController joystick = new CommandXboxController(0);
+    private final Joystick joystick1 = new Joystick(0);
+    private final Joystick joystick2 = new Joystick(1);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
@@ -54,25 +51,21 @@ public class RobotContainer {
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
                 // Drivetrain will execute this command periodically
+
                 drivetrain.applyRequest(
                         () ->
                                 drive.withVelocityX(
-                                                -joystick.getLeftY()
-                                                        * MaxSpeed) // Drive forward with
+                                                -joystick1.getY() * MaxSpeed) // Drive forward with
                                         // negative Y
                                         // (forward)
                                         .withVelocityY(
-                                                -joystick.getLeftX() * MaxSpeed) // Drive left with
+                                                -joystick1.getX() * MaxSpeed) // Drive left with
                                         // negative X
                                         // (left)
                                         .withRotationalRate(
-                                                -joystick.getRightX() * MaxAngularRate) // Drive
-                        // counterclockwise
-                        // with negative X (left)
+                                                -joystick2.getDirectionDegrees()
+                                                        * MaxAngularRate) // Rotate
                         ));
-
-        // reset the field-centric heading on left bumper press
-        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         drivetrain.registerTelemetry(logger::telemeterize);
     }
