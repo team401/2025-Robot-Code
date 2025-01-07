60eaaf8cf5683ec7347ad7485a8aa73da1433562
minhnguyenbhs <minhnguyen@mcps.org>
2025-01-07 13:05:36 -0500

    vaguely getting the hang of it, module code is close to done (i just need a few values from design, in theory); not super sure what is happening with gyro timestamps atm

---
+++ src/main/java/frc/robot/subsystems/drive/GyroIOSim.java
@@ -0,0 +1,43 @@
+package frc.robot.subsystems.drive;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusCode;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.Pigeon2Configuration;
+import com.ctre.phoenix6.hardware.Pigeon2;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import frc.robot.generated.TunerConstants;
+
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import java.util.Queue;
+
+import org.ironmaple.simulation.drivesims.GyroSimulation;
+
+/** IO implementation for Pigeon 2. */
+public class GyroIOSim implements GyroIO {
+  private final GyroSimulation gyroSimulation;
+  
+  public GyroIOSim(GyroSimulation gyroSimulation) {
+    this.gyroSimulation = gyroSimulation;
+  }
+
+  @Override
+  public void updateInputs(GyroIOInputs inputs) {
+    inputs.connected = true;
+    inputs.yawPosition = gyroSimulation.getGyroReading();
+    inputs.yawVelocityRadPerSec = gyroSimulation.getMeasuredAngularVelocity().in(RadiansPerSecond);
+
+    inputs.odometryYawTimestamps =
+      yawTimestampQueue.stream().mapToDouble((Double value) -> value).toArray();
+    inputs.odometryYawPositions =
+        yawPositionQueue.stream()
+            .map((Double value) -> Rotation2d.fromDegrees(value))
+            .toArray(Rotation2d[]::new);
+    yawTimestampQueue.clear();
+    yawPositionQueue.clear();
+  }
+}
