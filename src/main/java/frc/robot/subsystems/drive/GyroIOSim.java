1bfd62a75e1b9d59865c7cd10f8086cab4c3f130
minhnguyenbhs <minhnguyen@mcps.org>
2025-01-09 18:21:49 -0500

    all I can do currently, not applying queues in gyro atm

--- src/main/java/frc/robot/subsystems/drive/GyroIOSim.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOSim.java
@@ -1,28 +1,23 @@
 package frc.robot.subsystems.drive;
 
-import com.ctre.phoenix6.BaseStatusSignal;
-import com.ctre.phoenix6.StatusCode;
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.configs.Pigeon2Configuration;
-import com.ctre.phoenix6.hardware.Pigeon2;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.AngularVelocity;
+import com.ctre.phoenix6.hardware.core.CorePigeon2;
+import com.ctre.phoenix6.sim.Pigeon2SimState;
+
 import frc.robot.generated.TunerConstants;
 
 import static edu.wpi.first.units.Units.RadiansPerSecond;
-
-import java.util.Queue;
 
 import org.ironmaple.simulation.drivesims.GyroSimulation;
 
 /** IO implementation for Pigeon 2. */
 public class GyroIOSim implements GyroIO {
   private final GyroSimulation gyroSimulation;
-  
+  private final Pigeon2SimState pigeon = new Pigeon2SimState(
+    new CorePigeon2(TunerConstants.DrivetrainConstants.Pigeon2Id));
+
   public GyroIOSim(GyroSimulation gyroSimulation) {
     this.gyroSimulation = gyroSimulation;
+    pigeon.setRawYaw(0.0);
   }
 
   @Override
@@ -31,13 +26,5 @@ public class GyroIOSim implements GyroIO {
     inputs.yawPosition = gyroSimulation.getGyroReading();
     inputs.yawVelocityRadPerSec = gyroSimulation.getMeasuredAngularVelocity().in(RadiansPerSecond);
 
-    inputs.odometryYawTimestamps =
-      yawTimestampQueue.stream().mapToDouble((Double value) -> value).toArray();
-    inputs.odometryYawPositions =
-        yawPositionQueue.stream()
-            .map((Double value) -> Rotation2d.fromDegrees(value))
-            .toArray(Rotation2d[]::new);
-    yawTimestampQueue.clear();
-    yawPositionQueue.clear();
   }
 }
