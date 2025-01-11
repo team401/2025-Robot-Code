10f1a7381513eb0bed9ef63bb22e2f5697a75223
nathansmith <gonathansmith@gmail.com>
2025-01-10 22:42:00 -0500

    I think I fixed the spotless problem

--- src/main/java/frc/robot/subsystems/StateMachineExample.java
+++ src/main/java/frc/robot/subsystems/StateMachineExample.java
@@ -1,97 +1,101 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import coppercore.controls.state_machine.StateMachine;
+import coppercore.controls.state_machine.StateMachineConfiguration;
 import coppercore.controls.state_machine.state.PeriodicStateInterface;
 import coppercore.controls.state_machine.state.StateContainer;
-import coppercore.controls.state_machine.StateMachineConfiguration;
-import coppercore.controls.state_machine.StateMachine;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class StateMachineExample extends SubsystemBase {
 
-   static class IdleState implements PeriodicStateInterface {
-      @Override
-      public void periodic(){
-         //Called in subsystem periodic if statemachine in this state.
-      }
-   };
-   static class ActionState implements PeriodicStateInterface {
-      @Override
-      public void periodic(){
-         //Called in subsystem periodic if statemachine in this state.
-      }
-   };
-   static class OverrideState implements PeriodicStateInterface {
-      @Override
-      public void periodic(){
-         //Called in subsystem periodic if statemachine in this state.
-      }
-   };
+    static class IdleState implements PeriodicStateInterface {
+        @Override
+        public void periodic() {
+            // Called in subsystem periodic if statemachine in this state.
+        }
+    }
+    ;
 
-   public static enum ExampleStateContainer implements StateContainer {
-      IDLE(new IdleState()),
-      ACTION(new ActionState()),
-      OVERRIDE(new OverrideState());
+    static class ActionState implements PeriodicStateInterface {
+        @Override
+        public void periodic() {
+            // Called in subsystem periodic if statemachine in this state.
+        }
+    }
+    ;
 
-      private final PeriodicStateInterface state;
+    static class OverrideState implements PeriodicStateInterface {
+        @Override
+        public void periodic() {
+            // Called in subsystem periodic if statemachine in this state.
+        }
+    }
+    ;
 
-      ExampleStateContainer(PeriodicStateInterface state) {
-         this.state = state;
-      }
+    public static enum ExampleStateContainer implements StateContainer {
+        IDLE(new IdleState()),
+        ACTION(new ActionState()),
+        OVERRIDE(new OverrideState());
 
-      @Override
-      public PeriodicStateInterface getState() {
-         return state;
-      }
-   }
+        private final PeriodicStateInterface state;
 
-   public static enum ExampleActions {
-      IDLE,
-      ACTION,
-      OVERRIDE
-   }
+        ExampleStateContainer(PeriodicStateInterface state) {
+            this.state = state;
+        }
 
-   private StateMachine<ExampleStateContainer, ExampleActions> stateMachine;
+        @Override
+        public PeriodicStateInterface getState() {
+            return state;
+        }
+    }
 
-   private ExampleActions currentAction = ExampleActions.IDLE;
+    public static enum ExampleActions {
+        IDLE,
+        ACTION,
+        OVERRIDE
+    }
 
-   public void setAction(ExampleActions action){
-      currentAction = action;
-   }
+    private StateMachine<ExampleStateContainer, ExampleActions> stateMachine;
 
-   public ExampleActions getAction(){
-      return currentAction;
-   }
+    private ExampleActions currentAction = ExampleActions.IDLE;
 
-   public void setupStateMachine(){
-      StateMachineConfiguration<ExampleStateContainer, ExampleActions> stateMachineConfig = new StateMachineConfiguration<>();
+    public void setAction(ExampleActions action) {
+        currentAction = action;
+    }
 
-      stateMachineConfig
-         .configure(ExampleStateContainer.IDLE)
-         .permit(ExampleActions.ACTION, ExampleStateContainer.ACTION)
-         .permit(ExampleActions.OVERRIDE, ExampleStateContainer.OVERRIDE);
+    public ExampleActions getAction() {
+        return currentAction;
+    }
 
-      stateMachineConfig
-         .configure(ExampleStateContainer.ACTION)
-         .permit(ExampleActions.IDLE, ExampleStateContainer.IDLE)
-         .permit(ExampleActions.OVERRIDE, ExampleStateContainer.OVERRIDE);
+    public void setupStateMachine() {
+        StateMachineConfiguration<ExampleStateContainer, ExampleActions> stateMachineConfig =
+                new StateMachineConfiguration<>();
 
-      stateMachineConfig
-         .configure(ExampleStateContainer.OVERRIDE)
-         .permit(ExampleActions.ACTION, ExampleStateContainer.ACTION)
-         .permit(ExampleActions.IDLE, ExampleStateContainer.IDLE);
+        stateMachineConfig
+                .configure(ExampleStateContainer.IDLE)
+                .permit(ExampleActions.ACTION, ExampleStateContainer.ACTION)
+                .permit(ExampleActions.OVERRIDE, ExampleStateContainer.OVERRIDE);
 
+        stateMachineConfig
+                .configure(ExampleStateContainer.ACTION)
+                .permit(ExampleActions.IDLE, ExampleStateContainer.IDLE)
+                .permit(ExampleActions.OVERRIDE, ExampleStateContainer.OVERRIDE);
 
-      stateMachine = new StateMachine<>(stateMachineConfig, ExampleStateContainer.IDLE);
-   }
-   
-   public StateMachineExample(){
-      setupStateMachine();
-   }
-   
-   @Override
-   public void periodic() {
-      stateMachine.fire(currentAction);
-      stateMachine.getCurrentState().getState().periodic();
-   }
-   
+        stateMachineConfig
+                .configure(ExampleStateContainer.OVERRIDE)
+                .permit(ExampleActions.ACTION, ExampleStateContainer.ACTION)
+                .permit(ExampleActions.IDLE, ExampleStateContainer.IDLE);
+
+        stateMachine = new StateMachine<>(stateMachineConfig, ExampleStateContainer.IDLE);
+    }
+
+    public StateMachineExample() {
+        setupStateMachine();
+    }
+
+    @Override
+    public void periodic() {
+        stateMachine.fire(currentAction);
+        stateMachine.getCurrentState().getState().periodic();
+    }
 }
