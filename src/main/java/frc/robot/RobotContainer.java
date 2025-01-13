36e2bf8c9313636960bee573c4992bb6d13bcb70
aidnem <99768676+aidnem@users.noreply.github.com>
2025-01-12 22:14:24 -0500

    Add elevator (#10)
    * Add advantage kit dependency
    
    * Add elevator subsystem boilerplate and base elevator IO
    
    * Break constants into their own files and add basic talon FX configs
    
    * Refactor TalonFX configs to use units library properly
    
    * Add chinese remainder theorem code
    
    * Make elevator inputs and outputs mutable
    
    * Move Chinese Remainder Theorem out of the IO and into Elevator Mechanism
    
    * Implement the rest of ElevatorIOTalonFX except for controlling to a position
    
    * Generalize elevator gear ratios instead of hardcoding
    
    * Make elevator IO apply requests to control to position
    
    * Add conversion from goal height to goal rotations in ElevatorMechanism
    
    * Fix small encoder ticks being hardcoded in loop bounds for CRT math
    
    * WIP - Untested - Add basic phoenix6 based elevator sim
    
    * WIP - Add featureflags and enough boilerplate to robot container for sim to run, fix errors so that sim no longer crashes, add spotless. Sim updates not being updated properly it seems
    
    * Everything moves very slowly but sim appears to work now
    
    * Fix gear ratios and move to control in large CANCoder space instead of spool
    
    * Add gravity back to elevator sim and update expo constants to make sim actually show expo capabilities
    
    * Add tuning mode with LoggedTunableNumber to elevator mechanism
    
    * Remove ExampleSubsystem and auto
    
    * Add InitSubsystems.java and move elevator init there
    
    * Cleanup ElevatorIOSim and break constants out into SimConstants and ElevatorConstants.Sim
    
    * Only send goal height to elevator IO in periodic
    
    * Add missing doc strings to ElevatorIO
    
    * Remove dead code and add missing comments to ElevatorIOTalonFX
    
    * Add coppercore dependency and make ElevatorMechanism use UnitUtils.clampMeasure instead of manually clamping.
    
    * WIP - Try to add example elevator command
    
    * Add a ton more logging, fix sim functionality but motor ratio is technically physically inaccurate
    
    * Re-enable example elevator command
    
    * No longer manually command elevator to 0.75 meters before initializing command
    
    * Make FeatureFlags use JSONSync
    
    * Move OperatorConstants, ElevatorConstants, and ElevatorConstants.Sim to use JSONSync
    
    * Add TestModeManager to handle test mode choosing
    
    * added loggedtunablenumbers- can you take a look thanks
    
    * Make controls import use coppercoreVersion variable
    
    * Update ElevatorMechanism to import LoggedTunableNumber from parameter_tools
    
    * Migrate project to wpilib 2025.1.1 coppercore 2025.0.0 advantagekit 4.0.0 phoenix6 2025.1.0
    
    * Re-add spotless after migration removed it from build.gradle
    
    * Format
    
    ---------
    
    Co-authored-by: minhnguyenbhs <minhnguyen@mcps.org>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,13 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.Autos;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.constants.ElevatorConstants;
+import frc.robot.constants.FeatureFlags;
+import frc.robot.constants.OperatorConstants;
+import frc.robot.subsystems.elevator.ElevatorSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -19,45 +19,59 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    // The robot's subsystems and commands are defined here
+    private ElevatorSubsystem elevatorSubsystem;
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+    // Replace with CommandPS4Controller or CommandJoystick if needed
+    private final CommandXboxController m_driverController =
+            new CommandXboxController(OperatorConstants.synced.getObject().kDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the trigger bindings
-    configureBindings();
-  }
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() {
+        loadConstants();
+        configureSubsystems();
+        // Configure the trigger bindings
+        configureBindings();
+    }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    public void loadConstants() {
+        FeatureFlags.synced.loadData();
+        OperatorConstants.synced.loadData();
+        ElevatorConstants.synced.loadData();
+        ElevatorConstants.Sim.synced.loadData();
+    }
 
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
-  }
+    public void configureSubsystems() {
+        if (FeatureFlags.synced.getObject().runElevator) {
+            elevatorSubsystem = InitSubsystems.initElevatorSubsystem();
+        }
+    }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+    /**
+     * Use this method to define your trigger->command mappings. Triggers can be created via the
+     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+     * predicate, or via the named factories in {@link
+     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+     * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+     * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+     * joysticks}.
+     */
+    private void configureBindings() {}
+
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        // An example command will be run in autonomous
+        return null;
+    }
+
+    /** This method must be called from the robot, as it isn't called automatically. */
+    public void testPeriodic() {
+        if (FeatureFlags.synced.getObject().runElevator) {
+            elevatorSubsystem.testPeriodic();
+        }
+    }
 }
