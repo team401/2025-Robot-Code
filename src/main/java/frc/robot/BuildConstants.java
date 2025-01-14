c9e7b028851fb71dfa4a9b8043e4167537eaf22a
aidnem <99768676+aidnem@users.noreply.github.com>
2025-01-13 20:02:07 -0500

    Change team number

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2025-Robot-Code";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 37;
-    public static final String GIT_SHA = "4df2818d54b5630895717c5e971271da13a4d0b6";
-    public static final String GIT_DATE = "2025-01-13 19:31:22 EST";
+    public static final int GIT_REVISION = 38;
+    public static final String GIT_SHA = "b325103d450f8ba1061e2817c94f217be2f36dfb";
+    public static final String GIT_DATE = "2025-01-13 19:42:28 EST";
     public static final String GIT_BRANCH = "phoenix-6-drivetrain";
-    public static final String BUILD_DATE = "2025-01-13 19:40:47 EST";
-    public static final long BUILD_UNIX_TIME = 1736815247793L;
+    public static final String BUILD_DATE = "2025-01-13 19:54:01 EST";
+    public static final long BUILD_UNIX_TIME = 1736816041762L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
