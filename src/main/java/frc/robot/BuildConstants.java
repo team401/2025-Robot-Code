301f953607f35145424f4edf98de47c893baffa2
Jack Lingle <captainjack0818@gmail.com>
2025-01-21 08:35:47 -0500

    layout command structure

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -6,11 +6,11 @@ public final class BuildConstants {
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
   public static final int GIT_REVISION = 10;
-  public static final String GIT_SHA = "9d403ebfc0e93bb0b41e4b039f176b7b894d4414";
-  public static final String GIT_DATE = "2025-01-21 08:22:37 EST";
+  public static final String GIT_SHA = "8c6e24605a460f559a582422799002eed963ed6c";
+  public static final String GIT_DATE = "2025-01-21 08:23:19 EST";
   public static final String GIT_BRANCH = "otf-drive-command";
-  public static final String BUILD_DATE = "2025-01-21 08:23:12 EST";
-  public static final long BUILD_UNIX_TIME = 1737465792099L;
+  public static final String BUILD_DATE = "2025-01-21 08:34:23 EST";
+  public static final long BUILD_UNIX_TIME = 1737466463542L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/commands/drive/DriveWithJoysticksReefscape.java
+++ src/main/java/frc/robot/commands/drive/DriveWithJoysticksReefscape.java
@@ -5,6 +5,10 @@ import coppercore.wpilib_interface.DriveWithJoysticks;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 
 public class DriveWithJoysticksReefscape extends DriveWithJoysticks {
+  private CommandJoystick leftJoystick;
+  private CommandJoystick rightJoystick;
+  private double joystickDeadband;
+
   public DriveWithJoysticksReefscape(
       DriveTemplate drive,
       CommandJoystick leftJoystick,
@@ -19,5 +23,29 @@ public class DriveWithJoysticksReefscape extends DriveWithJoysticks {
         maxLinearVelocity,
         maxAngularVelocity,
         joystickDeadband);
+
+    this.leftJoystick = leftJoystick;
+    this.rightJoystick = rightJoystick;
+    this.joystickDeadband = joystickDeadband;
+  }
+
+  @Override
+  public void execute() {
+    if (areJoysticksActive()) {
+      super.execute();
+    } else {
+      // check for OTF 
+    }
+  }
+
+  /**
+   * checks if input is being received from joysticks
+   * 
+   * @return true if inputs are greater than the deadband on any joystick
+   */
+  public boolean areJoysticksActive() {
+    return leftJoystick.getX() > joystickDeadband
+        || leftJoystick.getY() > joystickDeadband
+        || rightJoystick.getX() > joystickDeadband;
   }
 }
