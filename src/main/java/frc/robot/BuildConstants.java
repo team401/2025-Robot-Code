85ec82769417c0ea505382f2c840ec73c8fda809
aidnem <99768676+aidnem@users.noreply.github.com>
2025-01-23 13:00:05 -0500

    Fix multi-platform stuff

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 9;
-  public static final String GIT_SHA = "d848c533bc3726782dca007d604a8c12e4b1238f";
-  public static final String GIT_DATE = "2025-01-20 14:29:55 EST";
+  public static final int GIT_REVISION = 10;
+  public static final String GIT_SHA = "1d439da9a73ae8fcdb90c256999f98dcb86b8a2c";
+  public static final String GIT_DATE = "2025-01-23 12:07:40 EST";
   public static final String GIT_BRANCH = "48-multi-platform-json";
-  public static final String BUILD_DATE = "2025-01-23 12:03:23 EST";
-  public static final long BUILD_UNIX_TIME = 1737651803366L;
+  public static final String BUILD_DATE = "2025-01-23 12:57:27 EST";
+  public static final long BUILD_UNIX_TIME = 1737655047581L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
