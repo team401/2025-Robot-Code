ca62a8c267aa9a262abd813b24155a07149dd489
Jack Lingle <captainjack0818@gmail.com>
2025-01-23 11:45:12 -0500

    add OTF pathfollow to drive (#46)
    * add DriveWithJoysticksReefscape command extending DriveWithJoysticks
    
    * layout command structure
    
    * add OTFLocations enum
    
    * add location enum, otf boolean, and findPose method to help with pathfinding
    
    * add abs value around joysticks to account for -1 to 1 values
    
    * add getDriveToPoseCommand method which gets a command based on desiredLocation
    
    * add examples of lineup paths
    
    * add docstrings, isDriveOTF method, and logic for OTF to command
    
    * format code
    
    * add exampel button binding for reef 0
    
    * rename PathLocation to OTFLocation
    
    * remove 2025 drive with joysticks command
    
    * add joystick trigger command to control otf
    
    * move bindings out of robot container, add desireed location selector helper command
    
    * add comment relating to warmup command
    
    * simplify DesiredLocationSelector by adding method based on number for drive
    
    * fix command logic
    
    * unsure why path finding command is loop overrunning (this is causing drive to not folow)
    
    * add logs
    
    * add coralstatin left and right to locations, add poses for them
    
    * format

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "8cddb88881e570b206c8abfbe5f7f93c6cc70704";
-  public static final String GIT_DATE = "2025-01-22 17:43:16 EST";
-  public static final String GIT_BRANCH = "add-drivetrain-sysid";
-  public static final String BUILD_DATE = "2025-01-22 19:08:51 EST";
-  public static final long BUILD_UNIX_TIME = 1737590931418L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 31;
+  public static final String GIT_SHA = "e6be8a4baf621a5b0476e475b02749a0c449ee23";
+  public static final String GIT_DATE = "2025-01-23 11:37:07 EST";
+  public static final String GIT_BRANCH = "otf-drive-command";
+  public static final String BUILD_DATE = "2025-01-23 11:38:46 EST";
+  public static final long BUILD_UNIX_TIME = 1737650326826L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
