8703c5d2ff9ebe687584c1ae62cf7c5c14b13f6f
Jack Lingle <captainjack0818@gmail.com>
2025-01-23 19:44:27 -0500

    add basic methods for help with lining up

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 31;
-  public static final String GIT_SHA = "e6be8a4baf621a5b0476e475b02749a0c449ee23";
-  public static final String GIT_DATE = "2025-01-23 11:37:07 EST";
-  public static final String GIT_BRANCH = "otf-drive-command";
-  public static final String BUILD_DATE = "2025-01-23 11:38:46 EST";
-  public static final long BUILD_UNIX_TIME = 1737650326826L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "76a85ad9be7940f1169af9a7faf611461e757c97";
+  public static final String GIT_DATE = "2025-01-23 17:57:38 EST";
+  public static final String GIT_BRANCH = "landing-zone-alignment";
+  public static final String BUILD_DATE = "2025-01-23 19:39:27 EST";
+  public static final long BUILD_UNIX_TIME = 1737679167823L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
