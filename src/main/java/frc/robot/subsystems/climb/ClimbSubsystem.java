98531ebaff14b7a12fc10429615cdf6dfa0c4ef6
minhnguyenbhs <minhnguyen@mcps.org>
2025-01-27 17:44:51 -0500

    you do not understand the extent to which I hate this

--- src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbSubsystem.java
@@ -8,116 +8,107 @@ import frc.robot.constants.ClimbConstants;
 import java.util.function.BooleanSupplier;
 import org.littletonrobotics.junction.Logger;
 
+import coppercore.controls.state_machine.StateMachine;
+import coppercore.controls.state_machine.StateMachineConfiguration;
+import coppercore.controls.state_machine.state.PeriodicStateInterface;
+import coppercore.controls.state_machine.state.StateContainer;
+
 public class ClimbSubsystem extends SubsystemBase {
 
     private ClimbIO io;
     private ClimbInputsAutoLogged inputs = new ClimbInputsAutoLogged();
     private ClimbOutputsAutoLogged outputs = new ClimbOutputsAutoLogged();
 
-    private ClimbAction Action = ClimbAction.NONE;
-    private ClimbState State = ClimbState.IDLE;
-
     private BooleanSupplier rampClear = () -> true;
 
     public enum ClimbAction {
         NONE, // do nothing
-        CLIMB, // start automated climb sequence
+        START_CLIMB, // start automated climb sequence
+        SYSTEM_READY,
+        READY_FOR_CLIMB,
         OVERRIDE
     }
 
-    private enum ClimbState {
-        IDLE, // do nothing
+    ClimbAction currentAction = ClimbAction.NONE;
+
+    private enum ClimbState implements StateContainer {
+        IDLE(ClimbSubsystem.idle()), // do nothing
         WAITING, // waiting for system to be ready for climb
         SEARCHING, // searching for target
         LIFTING, // moving arm to hang
-        OVERRIDE
+        OVERRIDE;
+
+        @Override
+        public PeriodicStateInterface getState() {
+            return state;
+        }
     }
+
+    public static StateMachineConfiguration<ClimbState, ClimbAction> climbMachineConfiguration;
+    public static StateMachine<ClimbState, ClimbAction> climbMachine;
 
     public ClimbSubsystem(ClimbIO io) {
         this.io = io;
+
+        climbMachineConfiguration = new StateMachineConfiguration<>();
+
+        climbMachineConfiguration
+                .configure(ClimbState.IDLE)
+                .permit(ClimbAction.START_CLIMB, ClimbState.WAITING)
+                .permit(ClimbAction.OVERRIDE, ClimbState.OVERRIDE);
+
+        climbMachineConfiguration
+                .configure(ClimbState.WAITING)
+                .permit(ClimbAction.SYSTEM_READY, ClimbState.SEARCHING)
+                .permit(ClimbAction.NONE, ClimbState.IDLE);
+
+        climbMachineConfiguration
+                .configure(ClimbState.SEARCHING)
+                .permit(ClimbAction.READY_FOR_CLIMB, ClimbState.LIFTING)
+                .permit(ClimbAction.NONE, ClimbState.IDLE);
+
+        climbMachineConfiguration
+                .configure(ClimbState.LIFTING)
+                .permit(ClimbAction.NONE, ClimbState.IDLE);
+
+        climbMachine = new StateMachine(climbMachineConfiguration, ClimbAction.NONE);
+
     }
 
     public void setAction(ClimbAction action) {
-        Action = action;
+        currentAction = action;
     }
 
     private void idle() {
-        io.setGoalAngle(ClimbConstants.restingAngle);
-
-        if (Action == ClimbAction.CLIMB) {
-            State = ClimbState.WAITING;
-        } else if (Action == ClimbAction.OVERRIDE) {
-            State = ClimbState.OVERRIDE;
-        }
     }
 
     private void waiting() {
-        if (Action != ClimbAction.CLIMB) {
-            State = ClimbState.IDLE;
-        } else if (rampClear.getAsBoolean()) {
-            State = ClimbState.SEARCHING;
-        } else {
-            io.setGoalAngle(ClimbConstants.restingAngle);
-        }
+        io.setGoalAngle(ClimbConstants.restingAngle);
     }
 
     private void searching() {
-        if (Action != ClimbAction.CLIMB) {
-            State = ClimbState.IDLE;
-        } else if (!rampClear.getAsBoolean()) {
-            State = ClimbState.WAITING;
-        } else if (inputs.lockedToCage) {
-            State = ClimbState.LIFTING;
-        } else {
-            io.setGoalAngle(ClimbConstants.searchingAngle);
-            // passive lock to bars
-            // drive forward?
-        }
+        io.setGoalAngle(ClimbConstants.searchingAngle);
     }
 
     private void lifting() {
-        if (Action != ClimbAction.CLIMB) {
-            State = ClimbState.IDLE;
-        } else if (!rampClear.getAsBoolean() || !inputs.lockedToCage) {
-            State = ClimbState.WAITING;
-        } else if (inputs.motorAngle.in(Degrees) > 20) {
-            io.setGoalAngle(ClimbConstants.finalHangingAngle);
-            // passive lock to bars
-        }
+        io.setGoalAngle(ClimbConstants.finalHangingAngle);
     }
 
     private void override() {
-        if (Action != ClimbAction.OVERRIDE) {
-            State = ClimbState.IDLE;
-        } else {
-            // idk not my top priority tbh
-        }
+        // idk not my top priority tbh
     }
 
     @Override
     public void periodic() {
 
+        climbMachine.fire(currentAction);
+
         io.updateInputs(inputs);
         io.applyOutputs(outputs);
 
-        switch (State) {
-            case WAITING:
-                waiting();
-                break;
-            case SEARCHING:
-                searching();
-                break;
-            case LIFTING:
-                lifting();
-                break;
-            default:
-                idle();
-                break;
-        }
-
         Logger.processInputs("climb/inputs", inputs);
         Logger.processInputs("climb/outputs", outputs);
-        Logger.recordOutput("climb/State", State);
-        Logger.recordOutput("climb/Action", Action);
+        Logger.recordOutput("climb/State", climbMachine.getCurrentState());
+        Logger.recordOutput("climb/Action", currentAction);
     }
 }
