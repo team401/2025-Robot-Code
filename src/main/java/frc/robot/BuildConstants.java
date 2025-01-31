a7079ac37506e17c9ec6f24913e89c872120a4e6
linglejack06 <captainjack0818@gmail.com>
2025-01-31 18:55:26 -0500

    is crashing on hotel needs to save

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2025-Robot-Code";
+  public static final String MAVEN_NAME = "2025-Robot-Code-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 53;
-  public static final String GIT_SHA = "038a586b469154bea20021c483c7f49143ecb6a8";
-  public static final String GIT_DATE = "2025-01-29 19:56:29 EST";
+  public static final int GIT_REVISION = 54;
+  public static final String GIT_SHA = "2cee222234f9b899156176f64012867e51cb35c3";
+  public static final String GIT_DATE = "2025-01-31 18:15:42 EST";
   public static final String GIT_BRANCH = "maplesim";
-  public static final String BUILD_DATE = "2025-01-31 18:13:06 EST";
-  public static final long BUILD_UNIX_TIME = 1738365186919L;
+  public static final String BUILD_DATE = "2025-01-31 18:44:52 EST";
+  public static final long BUILD_UNIX_TIME = 1738367092871L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/drive/ModuleIOMapleSim.java
+++ src/main/java/frc/robot/subsystems/drive/ModuleIOMapleSim.java
@@ -9,8 +9,8 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Current;
+import frc.robot.constants.*;
 import frc.robot.util.PhoenixUtil;
 import java.util.Arrays;
 import org.ironmaple.simulation.drivesims.SwerveModuleSimulation;
@@ -23,15 +23,6 @@ import org.ironmaple.simulation.motorsims.SimulatedMotorController;
  */
 public class ModuleIOMapleSim implements ModuleIO {
   // TunerConstants doesn't support separate sim constants, so they are declared locally
-  private static final double DRIVE_KP = 0.05;
-  private static final double DRIVE_KD = 0.0;
-  private static final double DRIVE_KS = 0.0;
-  private static final double DRIVE_KV_ROT =
-      0.91035; // Same units as TunerConstants: (volt * secs) / rotation
-  private static final double DRIVE_KV = 1.0 / Units.rotationsToRadians(1.0 / DRIVE_KV_ROT);
-  private static final double TURN_KP = 8.0;
-  private static final double TURN_KD = 0.0;
-
   // TODO:FIX
   private static final double DriveMotorGearRatio = 6.75;
   private static final double SteerMotorGearRatio = 25;
@@ -46,8 +37,16 @@ public class ModuleIOMapleSim implements ModuleIO {
 
   private boolean driveClosedLoop = false;
   private boolean turnClosedLoop = false;
-  private PIDController driveController = new PIDController(DRIVE_KP, 0, DRIVE_KD);
-  private PIDController turnController = new PIDController(TURN_KP, 0, TURN_KD);
+  private PIDController driveController =
+      new PIDController(
+          JsonConstants.drivetrainConstants.driveKp,
+          JsonConstants.drivetrainConstants.driveKi,
+          JsonConstants.drivetrainConstants.driveKd);
+  private PIDController turnController =
+      new PIDController(
+          JsonConstants.drivetrainConstants.steerKp,
+          JsonConstants.drivetrainConstants.steerKi,
+          JsonConstants.drivetrainConstants.steerKd);
   private double driveFFVolts = 0.0;
   private double driveAppliedVolts = 0.0;
   private double turnAppliedVolts = 0.0;
@@ -164,7 +163,9 @@ public class ModuleIOMapleSim implements ModuleIO {
   @Override
   public void setDriveVelocity(double velocityRadPerSec) {
     driveClosedLoop = true;
-    driveFFVolts = DRIVE_KS * Math.signum(velocityRadPerSec) + DRIVE_KV * velocityRadPerSec;
+    driveFFVolts =
+        JsonConstants.drivetrainConstants.driveKs * Math.signum(velocityRadPerSec)
+            + JsonConstants.drivetrainConstants.driveKv * velocityRadPerSec;
     driveController.setSetpoint(velocityRadPerSec);
   }
 
