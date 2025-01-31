25a583d1ec315510dec125ca9ec559fa4a8c222d
. <gonathansmith@gmail.com>
2025-01-30 19:49:55 -0500

    Why does this not work?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "954987f7bc59ce2888afc1376e12db70f87df620";
-  public static final String GIT_DATE = "2025-01-30 09:20:04 EST";
+  public static final int GIT_REVISION = 17;
+  public static final String GIT_SHA = "4526eb3b4287cd9d275c6389369f4539071dc464";
+  public static final String GIT_DATE = "2025-01-30 19:44:36 EST";
   public static final String GIT_BRANCH = "comp-integration";
-  public static final String BUILD_DATE = "2025-01-30 19:39:13 EST";
-  public static final long BUILD_UNIX_TIME = 1738283953365L;
+  public static final String BUILD_DATE = "2025-01-30 19:49:00 EST";
+  public static final long BUILD_UNIX_TIME = 1738284540277L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
