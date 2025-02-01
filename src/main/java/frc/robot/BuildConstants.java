1b04687c93d814af4a609df4f767716b152c6f1f
sleepyghost-zzz <joshua.azro@gmail.com>
2025-02-01 15:16:29 -0500

    Merged Main

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 24;
-  public static final String GIT_SHA = "7723d1e0e9ba4dfd756a6fcfcb93c439d395695c";
-  public static final String GIT_DATE = "2025-01-31 19:04:34 EST";
+  public static final int GIT_REVISION = 28;
+  public static final String GIT_SHA = "d4a1fbe81ce6942b228a0f13d78e18278338d7f3";
+  public static final String GIT_DATE = "2025-02-01 14:34:19 EST";
   public static final String GIT_BRANCH = "add-lock-on-reef";
-  public static final String BUILD_DATE = "2025-02-01 13:01:49 EST";
-  public static final long BUILD_UNIX_TIME = 1738432909974L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-01 14:36:08 EST";
+  public static final long BUILD_UNIX_TIME = 1738438568234L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
