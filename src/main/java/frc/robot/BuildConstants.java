9c241972a42508f4db86e16913e0069d9f3ec167
aidnem <99768676+aidnem@users.noreply.github.com>
2025-02-02 13:08:53 -0500

    Update JSON constants for Claw and ScoringFeatureFlags to reflect latest fields

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "954987f7bc59ce2888afc1376e12db70f87df620";
-  public static final String GIT_DATE = "2025-01-30 09:20:04 EST";
-  public static final String GIT_BRANCH = "comp-intergration-";
-  public static final String BUILD_DATE = "2025-02-01 14:02:58 EST";
-  public static final long BUILD_UNIX_TIME = 1738436578391L;
+  public static final int GIT_REVISION = 35;
+  public static final String GIT_SHA = "ca6e71977c9128a051d2e785d00fae7065c7a496";
+  public static final String GIT_DATE = "2025-02-02 13:02:56 EST";
+  public static final String GIT_BRANCH = "38-add-claw";
+  public static final String BUILD_DATE = "2025-02-02 13:05:45 EST";
+  public static final long BUILD_UNIX_TIME = 1738519545920L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
