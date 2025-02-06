d97be382028788f746e3f5ccac7bdc6ba8a89f2f
sleepyghost <145573597+sleepyghost-zzz@users.noreply.github.com>
2025-02-05 19:16:21 -0500

    Adds Angle Aligning (#58)
    * (WIP) Added Positions of POI's in FieldConstants.java
    
    * added angle aligner
    
    * changed to new coppercore ver
    
    * (WIP) Add Rotation Lock onto reef
    
    * Rotationally Locked In
    
    * Merge Part 2
    
    * JsonConstants fully and Changed file structure for constants
    
    * Spotless Build
    
    * Merged Main
    
    * pushing code
    
    * found my issue
    
    * Spotless Build
    
    * Merge Main
    
    * red updated locations for otf
    
    * add blue reef locations
    
    * update otf pose in drive
    
    * add coral station poses to constants
    
    * add locations to json
    
    * update findOTFPose method
    
    * adjust navgrid, and reef 45 rotation
    
    * update reef45 and reef89 rotations
    
    * remove json from init bindings
    
    * resolve
    
    * remove unecessary json files
    
    ---------
    
    Co-authored-by: Sojo123456 <henryolsen@mcps.org>
    Co-authored-by: linglejack06 <captainjack0818@gmail.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2025-Robot-Code";
+  public static final String MAVEN_NAME = "2025-Robot-Code-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 18;
-  public static final String GIT_SHA = "68678ee8f872b701c88c825bca64cbfb12edde36";
-  public static final String GIT_DATE = "2025-02-03 19:26:05 EST";
-  public static final String GIT_BRANCH = "68-public-elevatorIOs";
-  public static final String BUILD_DATE = "2025-02-04 08:56:30 EST";
-  public static final long BUILD_UNIX_TIME = 1738677390521L;
+  public static final int GIT_REVISION = 46;
+  public static final String GIT_SHA = "25f2cfcfd7a62559b6af5fc71c2258639513996c";
+  public static final String GIT_DATE = "2025-02-05 19:00:34 EST";
+  public static final String GIT_BRANCH = "add-lock-on-reef";
+  public static final String BUILD_DATE = "2025-02-05 19:11:03 EST";
+  public static final long BUILD_UNIX_TIME = 1738800663170L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
