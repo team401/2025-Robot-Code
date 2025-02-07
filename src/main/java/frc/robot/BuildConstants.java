4603c536acb6f29403d8b7d89900777f83557716
Sojo123456 <henryolsen@mcps.org>
2025-02-06 19:54:00 -0500

    still crashing into things that are not there

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "f39ac28dbf6c0dbeacefd2fcec56d3fb9ec3cc62";
-  public static final String GIT_DATE = "2025-02-06 19:31:59 EST";
+  public static final int GIT_REVISION = 67;
+  public static final String GIT_SHA = "01aa5bfeb2b08ec3ef57ab0bb216bc9bcd2c1dda";
+  public static final String GIT_DATE = "2025-02-06 19:40:44 EST";
   public static final String GIT_BRANCH = "maplesim";
-  public static final String BUILD_DATE = "2025-02-06 19:38:08 EST";
-  public static final long BUILD_UNIX_TIME = 1738888688709L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-02-06 19:52:01 EST";
+  public static final long BUILD_UNIX_TIME = 1738889521969L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -150,16 +150,19 @@ public class Robot extends LoggedRobot {
   /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {
+    SimulatedArena.getInstance().resetFieldForAuto();
     // SimulatedArena.overrideInstance(new Arena2025Reefscape());
   }
 
   /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {
-    if (ModeConstants.currentMode == frc.robot.constants.ModeConstants.Mode.SIM
-        || ModeConstants.currentMode == frc.robot.constants.ModeConstants.Mode.MAPLESIM) {
+    if (ModeConstants.currentMode == frc.robot.constants.ModeConstants.Mode.MAPLESIM) {
       SimulatedArena.getInstance().simulationPeriodic();
-      System.out.println(ModeConstants.currentMode);
+      Logger.recordOutput(
+          "FieldSimulation/Coral", SimulatedArena.getInstance().getGamePiecesArrayByType("Coral"));
+      Logger.recordOutput(
+          "FieldSimulation/Algae", SimulatedArena.getInstance().getGamePiecesArrayByType("Algae"));
     }
   }
 }
