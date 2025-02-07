621d2f155900ff9cc9952edeff5f6355f14a2ccc
Jack Lingle <captainjack0818@gmail.com>
2025-02-07 12:10:21 -0500

    add logic to auto commmand

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 29;
-  public static final String GIT_SHA = "ae105bc517bc52c67a94f6b56a78d6f01ebf15db";
-  public static final String GIT_DATE = "2025-02-07 11:48:33 EST";
+  public static final int GIT_REVISION = 30;
+  public static final String GIT_SHA = "55c12f3dee26d590ead32d524f00c132b793d5fe";
+  public static final String GIT_DATE = "2025-02-07 11:53:55 EST";
   public static final String GIT_BRANCH = "auto-command";
-  public static final String BUILD_DATE = "2025-02-07 11:53:39 EST";
-  public static final long BUILD_UNIX_TIME = 1738947219574L;
+  public static final String BUILD_DATE = "2025-02-07 12:10:09 EST";
+  public static final long BUILD_UNIX_TIME = 1738948209992L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/commands/AutoCommand.java
+++ src/main/java/frc/robot/commands/AutoCommand.java
@@ -1,81 +1,73 @@
-// package frc.robot.commands;
+package frc.robot.commands;
 
-// import edu.wpi.first.wpilibj2.command.Command;
-// import frc.robot.constants.JsonConstants;
-// import frc.robot.subsystems.drive.Drive;
-// import frc.robot.subsystems.drive.Drive.DesiredLocation;
-// import frc.robot.subsystems.drive.Drive.DriveTrigger;
-// import frc.robot.subsystems.scoring.ScoringSubsystem;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.strategies.AutoIntake;
+import frc.robot.commands.strategies.AutoScore;
+import frc.robot.constants.AutoPath;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.Drive.DesiredLocation;
+import frc.robot.subsystems.scoring.ScoringSubsystem;
 
-// public class AutoCommand extends Command {
-//   private Drive drive;
-//   private ScoringSubsystem scoringSubsystem;
+public class AutoCommand extends Command {
+  private Drive drive;
+  private ScoringSubsystem scoringSubsystem;
 
-//   private DesiredLocation currentScoringLocation;
-//   private int scoringLocationIndex = 0;
-//   private boolean shouldGoToIntake = false;
+  private DesiredLocation currentScoringLocation;
+  private int scoringLocationIndex = 0;
+  private boolean shouldGoToIntake = false;
+  private AutoPath path = null;
+  private AutoIntake intakeCommand = null;
+  private AutoScore scoreCommand = null;
 
-//   public AutoCommand(Drive drive, ScoringSubsystem scoring) {
-//     this.drive = drive;
-//     this.scoringSubsystem = scoring;
-//     this.currentScoringLocation =
-// JsonConstants.autoPath.scoringLocations.get(scoringLocationIndex);
+  public AutoCommand(Drive drive, ScoringSubsystem scoring, AutoPath path) {
+    this.drive = drive;
+    this.scoringSubsystem = scoring;
+    this.path = path;
+    this.currentScoringLocation = path.scoringLocations.get(scoringLocationIndex);
 
-//     addRequirements(drive, scoringSubsystem);
-//   }
+    addRequirements(drive, scoringSubsystem);
+  }
 
-//   public void initialize() {
-//     drive.setDesiredLocation(currentScoringLocation);
-//     drive.setDesiredIntakeLocation(JsonConstants.autoPath.intakeLocation);
-//     drive.fireTrigger(DriveTrigger.BeginAutoAlignment);
-//   }
+  public void initialize() {
+    intakeCommand = new AutoIntake(drive, scoringSubsystem, path.intakeLocation);
+    scoreCommand = new AutoScore(drive, scoringSubsystem, currentScoringLocation);
+  }
 
-//   /**
-//    * determines when everything has finished (scoring / intake and drive at location)
-//    *
-//    * @return true if we are ready for next path
-//    */
-//   public boolean isReadyForNextPath() {
-//     return drive.isDriveAlignmentFinished()
-//         && (shouldGoToIntake
-//             ? scoringSubsystem.shouldWaitOnIntake()
-//             : scoringSubsystem.shouldWaitOnIntake());
-//   }
+  /**
+   * checks if we have another path
+   *
+   * @return true if scoringLocationIndex + 1 is less than the size of locations
+   */
+  public boolean hasNextPath() {
+    return path.scoringLocations.size() > (scoringLocationIndex);
+  }
 
-//   /**
-//    * checks if we have another path
-//    *
-//    * @return true if scoringLocationIndex + 1 is less than the size of locations
-//    */
-//   public boolean hasNextPath() {
-//     return JsonConstants.autoPath.scoringLocations.size() > (scoringLocationIndex + 1);
-//   }
-
-//   /** sets next location for drive and fires trigger for auto alignment */
-//   public void prepareDrive() {
-//     drive.setGoToIntake(shouldGoToIntake);
-
-//     // if intake is false, go to next scoring location
-//     if (!shouldGoToIntake) {
-//       currentScoringLocation = JsonConstants.autoPath.scoringLocations.get(scoringLocationIndex);
-//       drive.setDesiredLocation(currentScoringLocation);
-//     }
-
-//     drive.fireTrigger(DriveTrigger.BeginAutoAlignment);
-//   }
-
-//   public void execute() {
-//     if (isReadyForNextPath()
-//         && hasNextPath()) { // if alignment finished, we move on to next location
-//       // toggle intake
-//       shouldGoToIntake = !shouldGoToIntake;
-//       // increment to next scoring (drive + score level)
-//       scoringLocationIndex++;
-//       prepareDrive();
-//     } else if (isReadyForNextPath() && !hasNextPath()) {
-//       // we are finished with scoring paths
-//       // TODO: should we go to intake one last time?
-//       this.cancel();
-//     }
-//   }
-// }
+  public void execute() {
+    if (shouldGoToIntake) {
+      if (intakeCommand.isFinished()) { // if we have finished intake, time to score
+        shouldGoToIntake = !shouldGoToIntake;
+        if (!hasNextPath()) { // if there is no scoring location left, we cancel
+          this.cancel();
+        }
+      } else if (!intakeCommand
+          .isScheduled()) { // if we are wanting to intake and not scheduled yet, schedule
+        intakeCommand.schedule();
+      }
+    } else { // wanting to score
+      if (scoreCommand.isFinished()) { // if scoring is finished, intake again
+        shouldGoToIntake = !shouldGoToIntake;
+        scoringLocationIndex++;
+        if (hasNextPath()) { // if there is a path, go ahead and get new command for next time
+          // around
+          currentScoringLocation = path.scoringLocations.get(scoringLocationIndex);
+          scoreCommand = new AutoScore(drive, scoringSubsystem, currentScoringLocation);
+        }
+      } else if (!scoreCommand.isScheduled()
+          && hasNextPath()) { // if its not scheduled and we have a path, run score
+        scoreCommand.schedule();
+      } else { // we have no score left, time to intake one last time
+        shouldGoToIntake = !shouldGoToIntake;
+      }
+    }
+  }
+}
