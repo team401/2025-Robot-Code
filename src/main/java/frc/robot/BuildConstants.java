19229e682f040be4c50c3635864c1f103d30b1f8
aidnem <99768676+aidnem@users.noreply.github.com>
2025-02-12 09:00:36 -0500

    Re-serialize elevator and wrist constants to include new fields, update the JSON files for comp and test_drivebase (using test_drivebase env for sim tuning), and remove scoring constants from HITL as it won't need them

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 22;
-  public static final String GIT_SHA = "0e9ea17f3fa1240610d68745df428963f0010655";
-  public static final String GIT_DATE = "2025-02-11 16:22:37 EST";
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "fbb9be6e60e52edb11e39c76586da0f9243a1544";
+  public static final String GIT_DATE = "2025-02-12 07:58:08 EST";
   public static final String GIT_BRANCH = "41-add-wrist";
-  public static final String BUILD_DATE = "2025-02-12 07:57:18 EST";
-  public static final long BUILD_UNIX_TIME = 1739365038889L;
+  public static final String BUILD_DATE = "2025-02-12 08:46:09 EST";
+  public static final long BUILD_UNIX_TIME = 1739367969040L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
