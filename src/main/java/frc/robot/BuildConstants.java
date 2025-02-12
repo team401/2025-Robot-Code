c99296ce07777d9cd60bfbb45a1e5f15a3f35b1c
avidraccoon <nathansmith@mcps.org>
2025-02-12 15:14:23 -0500

    Testing improved enviroment handler

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 32;
-  public static final String GIT_SHA = "afaf05dcfe19c59f2c5766b6878878932c454ed3";
-  public static final String GIT_DATE = "2025-02-12 14:39:45 EST";
+  public static final int GIT_REVISION = 33;
+  public static final String GIT_SHA = "bdc20c7a7b416a1ad351cf01257ac563465131bf";
+  public static final String GIT_DATE = "2025-02-12 14:51:32 EST";
   public static final String GIT_BRANCH = "37-basic-intake-functionality";
-  public static final String BUILD_DATE = "2025-02-12 14:42:54 EST";
-  public static final long BUILD_UNIX_TIME = 1739389374587L;
+  public static final String BUILD_DATE = "2025-02-12 15:14:09 EST";
+  public static final long BUILD_UNIX_TIME = 1739391249802L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
