5049b47b3cc6dec9ca74b4a5ddccb246a475fc28
aidnem <99768676+aidnem@users.noreply.github.com>
2025-02-12 17:16:02 -0500

    Remove unused field targets list from example auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 38;
-  public static final String GIT_SHA = "e7920269ad8169d43c0649b6fce6cb983c115d2e";
-  public static final String GIT_DATE = "2025-02-12 15:48:50 EST";
+  public static final int GIT_REVISION = 39;
+  public static final String GIT_SHA = "1c5331715a485247699a44669dbe513ea909e346";
+  public static final String GIT_DATE = "2025-02-12 17:02:18 EST";
   public static final String GIT_BRANCH = "41-add-wrist";
-  public static final String BUILD_DATE = "2025-02-12 17:01:43 EST";
-  public static final long BUILD_UNIX_TIME = 1739397703458L;
+  public static final String BUILD_DATE = "2025-02-12 17:15:06 EST";
+  public static final long BUILD_UNIX_TIME = 1739398506438L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
