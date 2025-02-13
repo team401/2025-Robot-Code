261b2472ffcfa4dce3a93011c60eddb47c7e00f7
minhnguyenbhs <minhnguyen@mcps.org>
2025-02-13 15:44:04 -0500

    files verified imported!

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,16 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2025-Robot-Code";
+  public static final String MAVEN_NAME = "2025-Robot-Code-1";
   public static final String VERSION = "unspecified";
-
-  public static final int GIT_REVISION = 68;
-  public static final String GIT_SHA = "4603c536acb6f29403d8b7d89900777f83557716";
-  public static final String GIT_DATE = "2025-02-06 19:54:00 EST";
-  public static final String GIT_BRANCH = "maplesim";
-  public static final String BUILD_DATE = "2025-02-08 16:04:11 EST";
-  public static final long BUILD_UNIX_TIME = 1739048651996L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 75;
+  public static final String GIT_SHA = "0465a7060cf32ac1f3666a7db5a6d42043cceb76";
+  public static final String GIT_DATE = "2025-02-08 17:36:08 EST";
+  public static final String GIT_BRANCH = "adding-cad";
+  public static final String BUILD_DATE = "2025-02-13 15:24:52 EST";
+  public static final long BUILD_UNIX_TIME = 1739478292562L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
