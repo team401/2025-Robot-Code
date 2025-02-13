fe2f240299a0431f87e6864c58d6d7ee3daa1fb7
minhnguyenbhs <minhnguyen@mcps.org>
2025-02-13 15:16:06 -0500

    all edits fixed i think!

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,27 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-<<<<<<< HEAD
   public static final String MAVEN_NAME = "2025-Robot-Code-1";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 45;
-  public static final String GIT_SHA = "08dfe2163665e41ff734f77046333d7ccdc45b24";
-  public static final String GIT_DATE = "2025-02-13 14:46:11 EST";
+  public static final int GIT_REVISION = 46;
+  public static final String GIT_SHA = "b9d7e1011f49107fd2c21f36ad723eca7e91c534";
+  public static final String GIT_DATE = "2025-02-13 15:12:29 EST";
   public static final String GIT_BRANCH = "36-basic-functionality-climb";
-  public static final String BUILD_DATE = "2025-02-13 15:11:56 EST";
-  public static final long BUILD_UNIX_TIME = 1739477516326L;
+  public static final String BUILD_DATE = "2025-02-13 15:15:10 EST";
+  public static final long BUILD_UNIX_TIME = 1739477710462L;
   public static final int DIRTY = 1;
-=======
-  public static final String MAVEN_NAME = "2025-Robot-Code";
-  public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 29;
-  public static final String GIT_SHA = "724f3e8de14c924bf23dcc87757eb17bf288557a";
-  public static final String GIT_DATE = "2025-02-13 13:14:42 EST";
-  public static final String GIT_BRANCH = "tune-along-track";
-  public static final String BUILD_DATE = "2025-02-13 13:15:25 EST";
-  public static final long BUILD_UNIX_TIME = 1739470525694L;
-  public static final int DIRTY = 0;
->>>>>>> main
 
   private BuildConstants() {}
 }
