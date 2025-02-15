32425e0cb3b5f4366de63e113d84a3cc2a49d96a
sleepyghost-zzz <joshua.azro@gmail.com>
2025-02-15 13:10:57 -0500

    Spotless Build

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 32;
-  public static final String GIT_SHA = "6be79192dfaa6bbef277e01d8d5e51d263a0a977";
-  public static final String GIT_DATE = "2025-02-15 12:42:49 EST";
+  public static final int GIT_REVISION = 33;
+  public static final String GIT_SHA = "de742ecd759c293e538bffc388f75656d0a1086e";
+  public static final String GIT_DATE = "2025-02-15 13:01:03 EST";
   public static final String GIT_BRANCH = "76-clean-up-drivetrain-constants";
-  public static final String BUILD_DATE = "2025-02-15 12:43:34 EST";
-  public static final long BUILD_UNIX_TIME = 1739641414640L;
+  public static final String BUILD_DATE = "2025-02-15 13:10:34 EST";
+  public static final long BUILD_UNIX_TIME = 1739643034196L;
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
