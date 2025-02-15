6e1206e9753aeb2d70b12a791df63e6df3a2b80e
avidraccoon <112419854+avidraccoon@users.noreply.github.com>
2025-02-15 15:16:14 -0500

    37 basic intake functionality (#65)
    * Start of Ramp Subsystem
    
    * Sim maybe working
    
    * Sim works but bad logic
    
    * Sim works but bad pid
    
    * .
    
    * Updates
    
    * Fix merge errors
    
    * Testing improved enviroment handler
    
    * fix problems
    
    * testing again
    
    * It works
    
    * Change copper core version
    
    * Fix problems
    
    * Fix spotless
    
    * Change binding
    
    * fix spotless
    
    * Actual Sim Values
    
    ---------
    
    Co-authored-by: nathansmith <gonathansmith@gmail.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 34;
-  public static final String GIT_SHA = "32425e0cb3b5f4366de63e113d84a3cc2a49d96a";
-  public static final String GIT_DATE = "2025-02-15 13:10:57 EST";
-  public static final String GIT_BRANCH = "76-clean-up-drivetrain-constants";
-  public static final String BUILD_DATE = "2025-02-15 13:15:07 EST";
-  public static final long BUILD_UNIX_TIME = 1739643307017L;
+  public static final int GIT_REVISION = 49;
+  public static final String GIT_SHA = "cfefaa60f49c800e917ea11daf8656bac97fbe17";
+  public static final String GIT_DATE = "2025-02-15 13:11:45 EST";
+  public static final String GIT_BRANCH = "37-basic-intake-functionality";
+  public static final String BUILD_DATE = "2025-02-15 14:55:16 EST";
+  public static final long BUILD_UNIX_TIME = 1739649316853L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
---
+++ src/main/java/frc/robot/subsystems/ramp/RampIO.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.ramp;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface RampIO {
+
+  @AutoLog
+  public static class RampInputs {
+    public double position = 0.0;
+    public boolean inPosition;
+  }
+
+  @AutoLog
+  public static class RampOutputs {
+    public double targetPosition;
+    public double appliedVolts;
+  }
+
+  public void updateInputs(RampInputs inputs);
+
+  public void updateOutputs(RampInputs inputs, RampOutputs outputs);
+}
