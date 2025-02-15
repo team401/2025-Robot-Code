85132a57d2f77f9eff2130e9353690f686aabc0d
sleepyghost <145573597+sleepyghost-zzz@users.noreply.github.com>
2025-02-15 14:43:42 -0500

    cleaned up drivetrain constants (#93)
    * Cleaning
    
    * Adjusted JSON constants to TestDrivebase
    
    * Spotless Build
    
    * Updated Comp and HITL values

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 28;
-  public static final String GIT_SHA = "e548533620981bc2bd6a90a74d9df246447bcb03";
-  public static final String GIT_DATE = "2025-02-13 16:09:44 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-14 11:15:57 EST";
-  public static final long BUILD_UNIX_TIME = 1739549757302L;
+  public static final int GIT_REVISION = 34;
+  public static final String GIT_SHA = "32425e0cb3b5f4366de63e113d84a3cc2a49d96a";
+  public static final String GIT_DATE = "2025-02-15 13:10:57 EST";
+  public static final String GIT_BRANCH = "76-clean-up-drivetrain-constants";
+  public static final String BUILD_DATE = "2025-02-15 13:15:07 EST";
+  public static final long BUILD_UNIX_TIME = 1739643307017L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
