86b7f31f968b8f6b473163bd20b26cbf4be92fae
minhnguyenbhs <minhnguyen@mcps.org>
2025-02-17 11:18:20 -0500

    led switch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -151,7 +151,6 @@ public class RobotContainer {
     strategyManager.addActionsFromAutoStrategy(autoChooser.getSelected());
 
     led.enabled(FeatureFlags.synced.getObject().runLEDs);
-  
   }
 
   public void teleopInit() {
