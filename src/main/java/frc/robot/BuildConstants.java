6060ca37c93c5bb47fc0ae62ddfd9a9f3ffbc55a
Sojo123456 <henryolsen@mcps.org>
2025-02-17 16:10:18 -0500

    update gains

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 33;
-  public static final String GIT_SHA = "7898a2a0710ebafed07abcde221d88d6eeb33a90";
-  public static final String GIT_DATE = "2025-02-17 14:01:36 EST";
+  public static final int GIT_REVISION = 34;
+  public static final String GIT_SHA = "ae5d5818b235f32a90e17f5b65de3fa1df3c58b2";
+  public static final String GIT_DATE = "2025-02-17 15:01:25 EST";
   public static final String GIT_BRANCH = "98-integrate-wrist";
-  public static final String BUILD_DATE = "2025-02-17 14:48:45 EST";
-  public static final long BUILD_UNIX_TIME = 1739821725632L;
+  public static final String BUILD_DATE = "2025-02-17 15:54:30 EST";
+  public static final long BUILD_UNIX_TIME = 1739825670362L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
