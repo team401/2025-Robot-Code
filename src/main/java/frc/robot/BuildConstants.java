7898a2a0710ebafed07abcde221d88d6eeb33a90
Jack Lingle <captainjack0818@gmail.com>
2025-02-17 14:01:36 -0500

    Camera updates (#97)
    * add hd resolutions 1280x720 and 1600x1200
    
    * add comp camera transforms
    
    * format

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 45;
-  public static final String GIT_SHA = "68998c4ac0babf18c179b57b7e8bdd2967f85d2a";
-  public static final String GIT_DATE = "2025-02-17 13:54:19 EST";
-  public static final String GIT_BRANCH = "74-tunable-elevator";
-  public static final String BUILD_DATE = "2025-02-17 13:55:17 EST";
-  public static final long BUILD_UNIX_TIME = 1739818517115L;
+  public static final int GIT_REVISION = 33;
+  public static final String GIT_SHA = "f89f85bfebfef45d9ba3f32a2c26e23417e6fe58";
+  public static final String GIT_DATE = "2025-02-17 13:54:13 EST";
+  public static final String GIT_BRANCH = "camera-updates";
+  public static final String BUILD_DATE = "2025-02-17 13:56:50 EST";
+  public static final long BUILD_UNIX_TIME = 1739818610497L;
+
   public static final int DIRTY = 0;
 
   private BuildConstants() {}
