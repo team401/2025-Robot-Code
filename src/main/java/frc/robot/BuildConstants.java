4763c4662b9b91b6df31341ffc49ed77c279fb59
aidnem <99768676+aidnem@users.noreply.github.com>
2025-02-19 16:42:53 -0500

    Update to coppercore 2025.1.11 for new features

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 38;
-  public static final String GIT_SHA = "2ec1681ecd827a99bcb6d1d96b478ea61060548a";
-  public static final String GIT_DATE = "2025-02-18 12:05:38 EST";
+  public static final int GIT_REVISION = 40;
+  public static final String GIT_SHA = "a794c6800fbd69f2a79f276fa7ca1e9baee01410";
+  public static final String GIT_DATE = "2025-02-19 13:53:01 EST";
   public static final String GIT_BRANCH = "98-integrate-wrist";
-  public static final String BUILD_DATE = "2025-02-18 16:02:58 EST";
-  public static final long BUILD_UNIX_TIME = 1739912578320L;
+  public static final String BUILD_DATE = "2025-02-19 16:37:32 EST";
+  public static final long BUILD_UNIX_TIME = 1740001052201L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
