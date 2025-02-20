590319231b0ba6261b43b4725764434aad5f25d1
avidraccoon <nathansmith@mcps.org>
2025-02-20 15:16:32 -0500

    ..

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 42;
-  public static final String GIT_SHA = "05518ae9889665c7e51c81aa19dc6d1bd51d2fc0";
-  public static final String GIT_DATE = "2025-02-17 13:05:26 EST";
+  public static final int GIT_REVISION = 43;
+  public static final String GIT_SHA = "a3f36b0c4c12630f6e22893270ab542e2dfc2727";
+  public static final String GIT_DATE = "2025-02-17 14:22:41 EST";
   public static final String GIT_BRANCH = "63-add-3d-robot-to-sim";
-  public static final String BUILD_DATE = "2025-02-17 14:14:21 EST";
-  public static final long BUILD_UNIX_TIME = 1739819661073L;
+  public static final String BUILD_DATE = "2025-02-18 18:40:13 EST";
+  public static final long BUILD_UNIX_TIME = 1739922013371L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
