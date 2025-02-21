16d3788f04e943a4253c7f645257e63df27edd97
Jack Lingle <captainjack0818@gmail.com>
2025-02-21 09:17:02 -0500

    add idle position and intake voltage fields to constants

--- src/main/java/frc/robot/constants/RampConstants.java
+++ src/main/java/frc/robot/constants/RampConstants.java
@@ -25,4 +25,9 @@ public class RampConstants {
   public final Double PID_SIM_P = 7.5;
   public final Double PID_SIM_I = 0.001;
   public final Double PID_SIM_D = 0.0;
+
+  public final double idlePosition = 1.0;
+
+  // TODO: tune this value
+  public final double intakeVoltage = 0.4; 
 }
