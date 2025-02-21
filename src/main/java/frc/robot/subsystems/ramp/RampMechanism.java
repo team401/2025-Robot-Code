222ab52c99bf8b09b99e95b62ebdc9c9973ee783
avidraccoon <nathansmith@mcps.org>
2025-02-21 11:02:32 -0500

    prepare for merge

--- src/main/java/frc/robot/subsystems/ramp/RampMechanism.java
+++ src/main/java/frc/robot/subsystems/ramp/RampMechanism.java
@@ -1,27 +1,65 @@
 package frc.robot.subsystems.ramp;
 
+import coppercore.parameter_tools.LoggedTunableNumber;
+import frc.robot.Robot;
+import frc.robot.TestModeManager;
 import frc.robot.constants.JsonConstants;
+
+import java.util.function.BooleanSupplier;
+
 import org.littletonrobotics.junction.Logger;
+
 
 public class RampMechanism {
   RampIO io;
-  public RampInputsAutoLogged inputs = new RampInputsAutoLogged();
+  RampInputsAutoLogged inputs = new RampInputsAutoLogged();
   RampOutputsAutoLogged outputs = new RampOutputsAutoLogged();
 
-  public double controlValue = 1.0;
-  public boolean inPosition = false;
-  public boolean positionControl = true;
-  public double positionRange = JsonConstants.rampConstants.positionRange;
+  private boolean positionControl = false;
+  private double controlValue = 0.0;
 
+  public BooleanSupplier inPositionBooleanSupplier;
+
+  LoggedTunableNumber tunablePosition;
+  LoggedTunableNumber tunableVoltage;
+  LoggedTunableNumber tunableKP;
+  LoggedTunableNumber tunableKI;
+  LoggedTunableNumber tunableKD;
+  
+  public void setInPositionSupplier(BooleanSupplier inPositionSupplier){
+    inPositionBooleanSupplier = inPositionSupplier;
+  }
+  
   public RampMechanism(RampIO io) {
-    this.io = io;
+     this.io = io;
+     tunablePosition = new LoggedTunableNumber("RampTunables/goalPos", controlValue);
+     tunableVoltage = new LoggedTunableNumber("RampTunables/voltage", controlValue);
+    final double defaultKP =
+        Robot.isReal()
+            ? JsonConstants.rampConstants.PID_TalonFX_P
+            : JsonConstants.rampConstants.PID_SIM_P;
+    final double defaultKI =
+        Robot.isReal()
+            ? JsonConstants.rampConstants.PID_TalonFX_I
+            : JsonConstants.rampConstants.PID_SIM_I;
+    final double defaultKD =
+        Robot.isReal()
+            ? JsonConstants.rampConstants.PID_TalonFX_D
+            : JsonConstants.rampConstants.PID_SIM_D;
+    tunableKP = new LoggedTunableNumber("RampTunables/kP", defaultKP);
+    tunableKI = new LoggedTunableNumber("RampTunables/kI", defaultKI);
+    tunableKD = new LoggedTunableNumber("RampTunables/kD", defaultKD);
   }
 
   public void periodic() {
+
+    inputs.positionControl = positionControl;
+    inputs.controlValue = controlValue;
+    
     io.updateInputs(inputs);
-    outputs.targetPosition = position;
-    inputs.inPosition = inPosition;
     io.updateOutputs(inputs, outputs);
+
+    Logger.recordOutput("ramp/inPosition", inPositionBooleanSupplier.getAsBoolean());
     Logger.processInputs("ramp/inputs", inputs);
     Logger.processInputs("ramp/outputs", outputs);
   }
@@ -36,11 +74,33 @@ public class RampMechanism {
     positionControl = false;
   }
 
-  public boolean inPosition() {
-    return inPosition;
+  public void testPeriodic() {
+    switch (TestModeManager.getTestMode()) {
+      case RampTuning:
+        LoggedTunableNumber.ifChanged(
+            hashCode(),
+            (pos) -> {
+              setPosition(pos[0]);
+            },
+            tunablePosition);
+        LoggedTunableNumber.ifChanged(
+            hashCode(),
+            (volt) -> {
+              setVoltage(volt[0]);
+            },
+            tunablePosition);
+        LoggedTunableNumber.ifChanged(
+            hashCode(),
+            (pid) -> {
+              io.setPID(pid[0], pid[1], pid[2]);
+            },
+            tunableKP,
+            tunableKI,
+            tunableKD);
+        break;
+      default:
+        break;
+    }
   }
 
-  public boolean inTransition() {
-    return !inPosition;
-  }
 }
