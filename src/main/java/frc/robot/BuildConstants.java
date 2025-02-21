a2c2fdd3d7442e78250c4387cf7324ae50c63f2d
avidraccoon <112419854+avidraccoon@users.noreply.github.com>
2025-02-20 21:18:49 -0500

    63 add 3d robot to sim (#94)
    * Mosty done
    
    * Elevator works with tempory solution in ScoringSubsystem
    
    * Make maplesim work with feature flags
    
    * .
    
    * .
    
    * Elevator not working
    
    * .
    
    * It works
    
    * MapleSim does not work still
    
    * Elevator works in maple sim
    
    * ..
    
    * fix
    
    * fix
    
    * .
    
    * maybe right?
    
    * Maybe works
    
    ---------
    
    Co-authored-by: avidraccoon <gonathansmith@gmail.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 33;
-  public static final String GIT_SHA = "f89f85bfebfef45d9ba3f32a2c26e23417e6fe58";
-  public static final String GIT_DATE = "2025-02-17 13:54:13 EST";
-  public static final String GIT_BRANCH = "camera-updates";
-  public static final String BUILD_DATE = "2025-02-17 13:56:50 EST";
-  public static final long BUILD_UNIX_TIME = 1739818610497L;
-
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 51;
+  public static final String GIT_SHA = "d8953319cd54f07c97093a704c71b0524db2c5db";
+  public static final String GIT_DATE = "2025-02-20 15:43:10 EST";
+  public static final String GIT_BRANCH = "63-add-3d-robot-to-sim";
+  public static final String BUILD_DATE = "2025-02-20 16:20:10 EST";
+  public static final long BUILD_UNIX_TIME = 1740086410871L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
