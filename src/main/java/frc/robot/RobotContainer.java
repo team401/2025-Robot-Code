9fcd4cbb73fe1b951ed6649ae0af0b10940b2514
avidraccoon <112419854+avidraccoon@users.noreply.github.com>
2025-02-21 08:57:13 -0500

    100 add ramp intake test mode (#112)
    * test mode made
    
    * .
    
    * Working tunable setpoint
    
    * Add PID to ramp tuning mode and make it actually run
    
    * Format
    
    ---------
    
    Co-authored-by: aidnem <99768676+aidnem@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -252,6 +252,10 @@ public class RobotContainer {
     if (FeatureFlags.synced.getObject().runScoring) {
       scoringSubsystem.testPeriodic();
     }
+
+    if (FeatureFlags.synced.getObject().runRamp) {
+      rampSubsystem.testPeriodic();
+    }
   }
 
   public void disabledPeriodic() {
