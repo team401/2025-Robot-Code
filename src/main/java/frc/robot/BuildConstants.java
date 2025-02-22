cbe370817697fbe573e5bd1be0640bad44f2cf0c
sleepyghost-zzz <joshua.azro@gmail.com>
2025-02-22 18:33:36 -0500

    I deleted json file

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 69;
-  public static final String GIT_SHA = "7023a0e44a13135edda2d22eda8e543db9a1bbdc";
-  public static final String GIT_DATE = "2025-02-22 16:48:51 EST";
+  public static final int GIT_REVISION = 70;
+  public static final String GIT_SHA = "c7396e3018763bd4b138821e34d9803b86bf6f21";
+  public static final String GIT_DATE = "2025-02-22 18:17:34 EST";
   public static final String GIT_BRANCH = "leds";
-  public static final String BUILD_DATE = "2025-02-22 18:16:45 EST";
-  public static final long BUILD_UNIX_TIME = 1740266205827L;
+  public static final String BUILD_DATE = "2025-02-22 18:30:47 EST";
+  public static final long BUILD_UNIX_TIME = 1740267047747L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
