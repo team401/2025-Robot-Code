01f19fb99db05ab0f18526fbef354a0e3d093007
aidnem <99768676+aidnem@users.noreply.github.com>
2025-02-26 16:43:40 -0500

    Format

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 43;
-  public static final String GIT_SHA = "746d4d5bd03b50bdfb23c28970e67c8d1bb0526c";
-  public static final String GIT_DATE = "2025-02-24 12:36:51 EST";
+  public static final int GIT_REVISION = 44;
+  public static final String GIT_SHA = "06806e0d8f04a349e218a1668f055fa320e38322";
+  public static final String GIT_DATE = "2025-02-24 18:17:20 EST";
   public static final String GIT_BRANCH = "131-scoring-velocity-threshold";
-  public static final String BUILD_DATE = "2025-02-24 18:16:09 EST";
-  public static final long BUILD_UNIX_TIME = 1740438969813L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-02-26 16:42:43 EST";
+  public static final long BUILD_UNIX_TIME = 1740606163277L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
