c9754a3256d8772cbd21607bd0d5be43ba738cf7
Jack Lingle <captainjack0818@gmail.com>
2025-02-28 11:00:14 -0500

    Ramp State machine (#114)
    * copy nathans code
    
    * add position control boolean to mechanism
    
    * add idle position and intake voltage fields to constants
    
    * .
    
    * prepare for merge
    
    * More changes
    
    * Even more changes
    
    * Homing State
    
    * Finishing homing
    
    * .
    
    * .
    
    * add scoring intake button
    
    * remove dT logic in favor of immediate stoppage (this confirm works)
    
    * add trigger fire for done intaking
    
    ---------
    
    Co-authored-by: avidraccoon <nathansmith@mcps.org>
    Co-authored-by: avidraccoon <112419854+avidraccoon@users.noreply.github.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2025-Robot-Code";
+  public static final String MAVEN_NAME = "2025-Robot-Code-2";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 47;
-  public static final String GIT_SHA = "d64013ab073a06ef916ed2b008a4179aa8d86c03";
-  public static final String GIT_DATE = "2025-02-27 15:46:33 EST";
-  public static final String GIT_BRANCH = "131-scoring-velocity-threshold";
-  public static final String BUILD_DATE = "2025-02-27 17:21:27 EST";
-  public static final long BUILD_UNIX_TIME = 1740694887448L;
+  public static final int GIT_REVISION = 58;
+  public static final String GIT_SHA = "cc3f85c269c847df140733c904a587872217652b";
+  public static final String GIT_DATE = "2025-02-27 19:29:48 EST";
+  public static final String GIT_BRANCH = "enhanced-ramp";
+  public static final String BUILD_DATE = "2025-02-27 19:49:52 EST";
+  public static final long BUILD_UNIX_TIME = 1740703792527L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
