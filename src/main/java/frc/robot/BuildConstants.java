40e661841e39454e182ae561cbf3a45eb671d0dc
aidnem <99768676+aidnem@users.noreply.github.com>
2025-03-01 10:11:49 -0500

    Try to make climb test mode work in sim but its still mostly broken

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 67;
-  public static final String GIT_SHA = "5bdcdc3c20e71df2a880beb5d20d042d3b8a643d";
-  public static final String GIT_DATE = "2025-02-28 23:52:35 EST";
+  public static final int GIT_REVISION = 68;
+  public static final String GIT_SHA = "3eff9d932a1a1fa3f9d791122dfbbc2a325a7537";
+  public static final String GIT_DATE = "2025-02-28 23:55:39 EST";
   public static final String GIT_BRANCH = "climb-test-mode";
-  public static final String BUILD_DATE = "2025-02-28 23:55:03 EST";
-  public static final long BUILD_UNIX_TIME = 1740804903028L;
+  public static final String BUILD_DATE = "2025-03-01 10:08:01 EST";
+  public static final long BUILD_UNIX_TIME = 1740841681824L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
