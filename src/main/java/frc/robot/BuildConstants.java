8f31b9fb8e0656631de366fdb76c2d65391f37cd
401-minh <93294337+minhnguyenbhs@users.noreply.github.com>
2025-03-02 12:49:55 -0800

    Climb Test Mode (#113)
    * erorr in jsonconstants, not sure why
    
    * unfortunately it won't let me edit multiple files at once on GitHub sorry for the many commits
    
    * climbconstants Json fix 2
    
    * climbconstants fix 3
    
    * removing rampsubsystem
    
    * removing rampsubsystem
    
    * remove isInClimbPosition()
    
    * still wip
    
    * fixed
    
    * adjusted the commands; the button for climb is now hit once for starting climb, a second time once its latched onto the bars
    no auto drive currently, the driver will have to drive into the bars themselves since there will be no sensor to detect latching onto the cage
    
    * changed a few things for aesthetic reasons (still waiting for climb to be finished)
    
    * wip
    
    * still not working
    
    * Fix canivore CAN issues, ran motors in phoenix tuner and they worked, didn't get to tune hardware because they took robot :(
    
    * tune kP and fix feedback sensors + add real setpoints
    
    * update setpoints in java + json
    
    * Climb integration (foot made it physically impossible ;-;)
    
    * Stop setting brake mode from breaking climb motor invert
    
    * Unify climb test mode in radians
    
    * Try to make climb test mode work in sim but its still mostly broken
    
    ---------
    
    Co-authored-by: Sojo123456 <henryolsen@mcps.org>
    Co-authored-by: aidnem <99768676+aidnem@users.noreply.github.com>
    Co-authored-by: linglejack06 <captainjack0818@gmail.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 48;
-  public static final String GIT_SHA = "118bdd4cb494f61fed89361ea54cde7de768ec66";
-  public static final String GIT_DATE = "2025-02-28 18:54:57 EST";
-  public static final String GIT_BRANCH = "138-real-autos";
-  public static final String BUILD_DATE = "2025-03-01 12:27:05 EST";
-  public static final long BUILD_UNIX_TIME = 1740850025964L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 68;
+  public static final String GIT_SHA = "3eff9d932a1a1fa3f9d791122dfbbc2a325a7537";
+  public static final String GIT_DATE = "2025-02-28 23:55:39 EST";
+  public static final String GIT_BRANCH = "climb-test-mode";
+  public static final String BUILD_DATE = "2025-03-01 10:08:01 EST";
+  public static final long BUILD_UNIX_TIME = 1740841681824L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
