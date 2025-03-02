160e646affdd5b5592165baf13438d19081aa6e1
Justin Kleiber <jkleiber8@gmail.com>
2025-03-02 18:04:18 -0500

    Tunes climb constants to get best hang (Drive by fix)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 66;
-  public static final String GIT_SHA = "6aeee78f33cf43d48cb8d6835134aa59cb926a7e";
-  public static final String GIT_DATE = "2025-03-02 13:55:25 EST";
+  public static final int GIT_REVISION = 69;
+  public static final String GIT_SHA = "72c5b23cd2069e3d31ff95372e9f7047fde02ccc";
+  public static final String GIT_DATE = "2025-03-02 17:54:38 EST";
   public static final String GIT_BRANCH = "lineup-tune";
-  public static final String BUILD_DATE = "2025-03-02 16:58:16 EST";
-  public static final long BUILD_UNIX_TIME = 1740952696190L;
+  public static final String BUILD_DATE = "2025-03-02 18:01:21 EST";
+  public static final long BUILD_UNIX_TIME = 1740956481078L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
