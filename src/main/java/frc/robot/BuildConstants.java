1eeb15d896e5746006ac236443c4f1ab0754eb50
Jack Lingle <captainjack0818@gmail.com>
2025-03-03 08:56:10 -0500

    format

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 53;
-  public static final String GIT_SHA = "d038c1e5febf69cd4852698eae9de8575d6af818";
-  public static final String GIT_DATE = "2025-03-03 08:32:33 EST";
+  public static final int GIT_REVISION = 56;
+  public static final String GIT_SHA = "3b36d0dc284e56121a2cf5a958503117eab8ecc7";
+  public static final String GIT_DATE = "2025-03-03 08:52:02 EST";
   public static final String GIT_BRANCH = "dynamic-obstacles";
-  public static final String BUILD_DATE = "2025-03-03 08:37:27 EST";
-  public static final long BUILD_UNIX_TIME = 1741009047186L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-03-03 08:56:01 EST";
+  public static final long BUILD_UNIX_TIME = 1741010161970L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
