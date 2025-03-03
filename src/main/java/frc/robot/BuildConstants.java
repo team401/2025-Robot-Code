64201d276dfa7ea987a6808e5a781f91d76db823
aidnem <99768676+aidnem@users.noreply.github.com>
2025-03-03 17:14:38 -0500

    Disable wrist being clamped up when holding an algae to avoid crushing algae into elevator

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 82;
-  public static final String GIT_SHA = "6e7f7cf3f4b2f4ba01a055f64dc013a6bde12644";
-  public static final String GIT_DATE = "2025-03-03 15:22:52 EST";
+  public static final int GIT_REVISION = 83;
+  public static final String GIT_SHA = "62ee3b95126502329df2748693aee6acc1dfe2c0";
+  public static final String GIT_DATE = "2025-03-03 16:37:02 EST";
   public static final String GIT_BRANCH = "lineup-tune";
-  public static final String BUILD_DATE = "2025-03-03 16:34:52 EST";
-  public static final long BUILD_UNIX_TIME = 1741037692987L;
+  public static final String BUILD_DATE = "2025-03-03 17:13:59 EST";
+  public static final long BUILD_UNIX_TIME = 1741040039637L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/scoring/ScoringSubsystem.java
+++ src/main/java/frc/robot/subsystems/scoring/ScoringSubsystem.java
@@ -737,7 +737,9 @@ public class ScoringSubsystem extends MonitoredSubsystem {
     Distance reefDistance = reefDistanceSupplier.get();
     Logger.recordOutput("scoring/reefDistanceSupplier", reefDistance);
     if (reefDistance.lt(JsonConstants.wristConstants.closeToReefThreshold)) {
-      if (elevatorHeight.lte(JsonConstants.elevatorConstants.minReefSafeHeight)) {
+      if (elevatorHeight.lte(JsonConstants.elevatorConstants.minReefSafeHeight)
+          && !isAlgaeDetected()) { // Don't slam wrist up if we're holding an algae, trust other
+        // clamps
         wristInToAvoidReefBase = true;
         // If elevator is next to reef base, make sure wrist doesn't hit it
         wristMinAngle.mut_replace(
@@ -745,10 +747,13 @@ public class ScoringSubsystem extends MonitoredSubsystem {
       }
 
       if (ReefAvoidanceHelper.willPassReefLevel(elevatorHeight, elevatorGoalHeight)) {
-        wristInToPassReef = true;
-        // If we will pass a reef level, clamp the wrist to be in a safe position to pass the reef
-        wristMinAngle.mut_replace(
-            (Angle) Measure.max(wristMinAngle, JsonConstants.wristConstants.minReefSafeAngle));
+        if (!isAlgaeDetected()) { // Don't slam wrist up if we're holding an algae, trust other
+          // clamps
+          wristInToPassReef = true;
+          // If we will pass a reef level, clamp the wrist to be in a safe position to pass the reef
+          wristMinAngle.mut_replace(
+              (Angle) Measure.max(wristMinAngle, JsonConstants.wristConstants.minReefSafeAngle));
+        }
 
         // If we will pass a reef level and the wrist is in an unsafe position to pass the reef,
         // clamp
