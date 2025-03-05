68850716598320d4a151ca7cd4ec68b8347da061
Jack Lingle <captainjack0818@gmail.com>
2025-03-05 10:42:06 -0500

    add other camera switch as check

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 60;
-  public static final String GIT_SHA = "753dbf96e29d940d4f0764c1312d3d9b32d51148";
-  public static final String GIT_DATE = "2025-03-05 09:21:57 EST";
+  public static final int GIT_REVISION = 61;
+  public static final String GIT_SHA = "7f920113a50d3e84e73901cdc30ce15d25b219ac";
+  public static final String GIT_DATE = "2025-03-05 10:13:38 EST";
   public static final String GIT_BRANCH = "comp-auto-integration";
-  public static final String BUILD_DATE = "2025-03-05 10:11:03 EST";
-  public static final long BUILD_UNIX_TIME = 1741187463638L;
+  public static final String BUILD_DATE = "2025-03-05 10:35:38 EST";
+  public static final long BUILD_UNIX_TIME = 1741188938700L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
