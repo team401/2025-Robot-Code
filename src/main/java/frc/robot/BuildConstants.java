c06f2c7588df7dacea4ee7797f148fc9d1646415
linglejack06 <captainjack0818@gmail.com>
2025-03-06 20:16:13 -0500

    update intake setpoint algae wrist

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2025-Robot-Code";
+  public static final String MAVEN_NAME = "2025-Robot-Code-3";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 56;
-  public static final String GIT_SHA = "eab20311f1e2f33b2a6d9b08969783a39d671bef";
-  public static final String GIT_DATE = "2025-03-06 12:32:28 EST";
+  public static final int GIT_REVISION = 57;
+  public static final String GIT_SHA = "248a59d89e4a335b9c5099c4bd1514b1682296b0";
+  public static final String GIT_DATE = "2025-03-06 16:28:30 EST";
   public static final String GIT_BRANCH = "154-mlg-algae";
-  public static final String BUILD_DATE = "2025-03-06 16:28:05 EST";
-  public static final long BUILD_UNIX_TIME = 1741296485224L;
+  public static final String BUILD_DATE = "2025-03-06 20:07:07 EST";
+  public static final long BUILD_UNIX_TIME = 1741309627371L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
