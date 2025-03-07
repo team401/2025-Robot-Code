111387e5ae21e986283b313f041dca4ce8137f41
aidnem <99768676+aidnem@users.noreply.github.com>
2025-03-07 12:23:55 -0500

    Actually run rollers when scoring algae into processor in ScoreState (#163)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 61;
-  public static final String GIT_SHA = "6ba3f48638f63674e547d62838f8603e05f036a3";
-  public static final String GIT_DATE = "2025-03-06 19:42:49 EST";
-  public static final String GIT_BRANCH = "elevator-setpoint-fix";
-  public static final String BUILD_DATE = "2025-03-07 08:29:10 EST";
-  public static final long BUILD_UNIX_TIME = 1741354150102L;
-
+  public static final int GIT_REVISION = 55;
+  public static final String GIT_SHA = "4730de9777efaed4bc8189514e5921f1de65ae25";
+  public static final String GIT_DATE = "2025-03-07 11:51:01 EST";
+  public static final String GIT_BRANCH = "162-processor-score-fix";
+  public static final String BUILD_DATE = "2025-03-07 12:05:51 EST";
+  public static final long BUILD_UNIX_TIME = 1741367151777L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
