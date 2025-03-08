491a6bfbac68bf32a0aaa0cf501a2df6cc6a8ade
sleepyghost-zzz <joshua.azro@gmail.com>
2025-03-08 12:07:24 -0500

    better rainbow and correct orientation

--- src/main/java/frc/robot/constants/LEDConstants.java
+++ src/main/java/frc/robot/constants/LEDConstants.java
@@ -13,9 +13,10 @@ import java.util.Map;
 public final class LEDConstants {
 
   public static final int ledPort = 9;
-  public static final int halfLength = 14; // alternate number to test = 504
-  public static final int totalLength = 28; // alternate number to test = 1008
-  public static final double rainbowSpeed = 50;
+  public static final int halfLength = 14;
+  public static final int totalLength = 28;
+  public static final int totalLengthRainbow = 150;
+  public static final double rainbowSpeed = 20;
 
   public static final Color off = Color.kBlack;
   // LED Patterns
