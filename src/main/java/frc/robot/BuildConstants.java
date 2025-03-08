7ddfe8a7b243f9b9f360e0da0229bdd0f5f84db2
linglejack06 <captainjack0818@gmail.com>
2025-03-08 09:54:43 -0500

    Update setpoints based on field calibration and practice 3

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,15 +3,15 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2025-Robot-Code";
+  public static final String MAVEN_NAME = "2025-Robot-Code-3";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 64;
-  public static final String GIT_SHA = "ef28704a1734243ec049e3562fc6f4649616b91f";
-  public static final String GIT_DATE = "2025-03-07 09:22:48 EST";
-  public static final String GIT_BRANCH = "switches";
-  public static final String BUILD_DATE = "2025-03-07 15:29:42 EST";
-  public static final long BUILD_UNIX_TIME = 1741379382842L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 58;
+  public static final String GIT_SHA = "6179038082ebc13fa12bfc01f3c9ce1af7bba7c1";
+  public static final String GIT_DATE = "2025-03-07 15:32:29 EST";
+  public static final String GIT_BRANCH = "VAPOR-setpoints";
+  public static final String BUILD_DATE = "2025-03-08 09:50:47 EST";
+  public static final long BUILD_UNIX_TIME = 1741445447574L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -173,7 +173,7 @@ public class RobotContainer {
         new File(Filesystem.getDeployDirectory().toPath().resolve("auto").toString());
     strategyContainer = new AutoStrategyContainer(autoDirectory.listFiles());
     for (AutoStrategy strategy : strategyContainer.getStrategies()) {
-      if (!firstDefault) {
+      if (strategy.autoStrategyName.equals("1PieceBarge")) {
         autoChooser.setDefaultOption(strategy.autoStrategyName, strategy);
         firstDefault = true;
       } else {
