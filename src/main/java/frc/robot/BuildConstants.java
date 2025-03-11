e48933290017a51ad8d1411f02f50849ec1391b2
minhnguyenbhs <minhnguyen@mcps.org>
2025-03-11 19:31:12 -0400

    wip

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,22 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-<<<<<<< HEAD
-  public static final int GIT_REVISION = 63;
-  public static final String GIT_SHA = "77da6e9c485e77439690e47e86740b5ea086ebc9";
-  public static final String GIT_DATE = "2025-02-28 19:54:39 EST";
-  public static final String GIT_BRANCH = "maplesim-intake";
-  public static final String BUILD_DATE = "2025-03-03 18:57:53 EST";
-  public static final long BUILD_UNIX_TIME = 1741046273024L;
-
-=======
   public static final int GIT_REVISION = 64;
   public static final String GIT_SHA = "aecf26c5772689087f5023b285dce9832e008e67";
   public static final String GIT_DATE = "2025-03-08 18:55:08 EST";
   public static final String GIT_BRANCH = "comp/VAPOR";
   public static final String BUILD_DATE = "2025-03-08 23:29:12 EST";
   public static final long BUILD_UNIX_TIME = 1741494552677L;
->>>>>>> origin/comp/VAPOR
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
