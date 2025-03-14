bc9c07230c9b736730fbc48fb8797f204baa55d7
aidnem <99768676+aidnem@users.noreply.github.com>
2025-03-14 10:58:49 -0400

    (#120) Log usingVariableL4 in scoringsubsystem periodic

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 67;
-  public static final String GIT_SHA = "9b91494679f5d03e64850d86a78a29904a3af8fd";
-  public static final String GIT_DATE = "2025-03-13 12:14:01 EDT";
+  public static final int GIT_REVISION = 68;
+  public static final String GIT_SHA = "0cbb9ef591a459877a4e77390b8c32c209330571";
+  public static final String GIT_DATE = "2025-03-13 23:42:47 EDT";
   public static final String GIT_BRANCH = "120-distance-based-setpoints";
-  public static final String BUILD_DATE = "2025-03-13 23:41:53 EDT";
-  public static final long BUILD_UNIX_TIME = 1741923713063L;
+  public static final String BUILD_DATE = "2025-03-14 10:58:20 EDT";
+  public static final long BUILD_UNIX_TIME = 1741964300890L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
