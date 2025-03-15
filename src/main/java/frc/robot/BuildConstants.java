ac6f5fce9956f89d41dd166f6be027b348a05e36
avidraccoon <112419854+avidraccoon@users.noreply.github.com>
2025-03-15 13:36:27 -0400

    Bouncy ramp (#179)
    * Restructure state machine for easier use and add new state to make ramp extend after the ramp homes on startup
    
    * Make RampStates.EXTENDING actually use the ExtendingState class, add state machine configureation for homing, and  make state machines initial state RampStates.STARTUP_HOMING.
    
    * format
    
    * Fix inPosition
    
    * Values work but need to be improved later
    
    * Fix feature flags

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 63;
-  public static final String GIT_SHA = "07c4908d74b1c52445c286bc8ccf96bb95080c8c";
-  public static final String GIT_DATE = "2025-03-14 11:43:52 EDT";
-  public static final String GIT_BRANCH = "174-smart-mode";
-  public static final String BUILD_DATE = "2025-03-14 12:25:03 EDT";
-  public static final long BUILD_UNIX_TIME = 1741969503490L;
+  public static final int GIT_REVISION = 65;
+  public static final String GIT_SHA = "b9352ffa982a2784ed3d704b6669d97fe78faa2d";
+  public static final String GIT_DATE = "2025-03-14 10:00:54 EDT";
+  public static final String GIT_BRANCH = "bouncy-ramp";
+  public static final String BUILD_DATE = "2025-03-14 18:35:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1741991746779L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
