5844b58598071bca677142111e22ba0e08fd508d
aidnem <99768676+aidnem@users.noreply.github.com>
2025-03-14 20:59:49 -0400

    Add smart autonomy mode with distance based nearest pole selection (#181)
    * (#174) Add distance based nearest pole selection & smart mode
    
    * (#174) Add missing undefined cases for Smart mode
    
    * (#174) Add intake support w/ automatic algae level selection to Smart autonomy mode

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 67;
-  public static final String GIT_SHA = "620d714148bb31c526199ae75defa6682a661301";
-  public static final String GIT_DATE = "2025-03-11 18:38:35 EDT";
-  public static final String GIT_BRANCH = "comp/VAPOR";
-  public static final String BUILD_DATE = "2025-03-13 00:32:02 EDT";
-  public static final long BUILD_UNIX_TIME = 1741840322327L;
+  public static final int GIT_REVISION = 63;
+  public static final String GIT_SHA = "07c4908d74b1c52445c286bc8ccf96bb95080c8c";
+  public static final String GIT_DATE = "2025-03-14 11:43:52 EDT";
+  public static final String GIT_BRANCH = "174-smart-mode";
+  public static final String BUILD_DATE = "2025-03-14 12:25:03 EDT";
+  public static final long BUILD_UNIX_TIME = 1741969503490L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
