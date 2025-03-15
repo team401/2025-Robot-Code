920afc1513b89a8ce6a806373041e8f7b2e9784a
Justin Kleiber <jkleiber8@gmail.com>
2025-03-15 17:18:27 -0400

    In shop testing of multiple OTF poses

--- src/main/java/frc/robot/StrategyManager.java
+++ src/main/java/frc/robot/StrategyManager.java
@@ -258,9 +258,9 @@ public class StrategyManager {
       // Don't automatically set level in smart mode; this will be set when the warmup trigger is
       // pressed. This can't happen in periodic because algae level is automatically determined when
       // intake is pressed and would be overridden by this in each loop.
-      if (getAutonomyMode() != AutonomyMode.Smart) {
-        scoringSubsystem.updateScoringLevelFromNetworkTables(reefLevelSelector.get());
-      }
+      // if (getAutonomyMode() != AutonomyMode.Smart) {
+      scoringSubsystem.updateScoringLevelFromNetworkTables(reefLevelSelector.get());
+      // }
     }
 
     // update autonomy level
