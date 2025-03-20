d5c406d2908dad887e7ee680b94615fbcd939da7
minhnguyenbhs <minhnguyen@mcps.org>
2025-03-19 20:04:13 -0400

    oops

--- src/main/java/frc/robot/subsystems/drive/states/PathFollowState.java
+++ src/main/java/frc/robot/subsystems/drive/states/PathFollowState.java
@@ -1,13 +1,18 @@
 package frc.robot.subsystems.drive.states;
 
+import java.io.IOException;
 import java.util.function.BiConsumer;
 
+import org.json.simple.parser.ParseException;
+
 import com.ctre.phoenix6.sim.ChassisReference;
+import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.FollowPathCommand;
 import com.pathplanner.lib.config.PIDConstants;
 import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.DriveFeedforwards;
+import com.pathplanner.lib.util.FileVersionException;
 
 import coppercore.controls.state_machine.state.PeriodicStateInterface;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
@@ -22,7 +27,7 @@ public class PathFollowState implements PeriodicStateInterface {
 
   public void periodic() {
 
-    FollowPathCommand(
+    /*FollowPathCommand(
         PathPlannerPath.fromPathFile("Test"), 
         () -> drive.getPose(), 
         () -> drive.getChassisSpeeds(), 
@@ -31,6 +36,13 @@ public class PathFollowState implements PeriodicStateInterface {
         },
         new PPHolonomicDriveController(
             new PIDConstants(1.0, 0.0, 0.0), new PIDConstants(5.0, 0.0, 0.0)), Drive.PP_CONFIG, 
-            () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red, drive);
+            () -> DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red, drive);*/
+      
+    try {
+      AutoBuilder.followPath(PathPlannerPath.fromPathFile("R0 Lineup"););
+    } catch (FileVersionException | IOException | ParseException e) {
+      e.printStackTrace();
+    }
+
   }
 }
