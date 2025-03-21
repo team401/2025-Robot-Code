dfe1609c70f9aea2d3b7297d4e6af882249cf6fb
minhnguyenbhs <minhnguyen@mcps.org>
2025-03-21 19:28:45 -0400

    Decrease ramp's voltage when flipping over to climb but it still works

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 73;
-  public static final String GIT_SHA = "2a0eff04a0924cc5191a7bef2405ad5dd5dc66d1";
-  public static final String GIT_DATE = "2025-03-20 11:35:24 EDT";
+  public static final int GIT_REVISION = 76;
+  public static final String GIT_SHA = "ec9ec0e8331b948fcb446e736cebc91f5f9976e0";
+  public static final String GIT_DATE = "2025-03-21 08:17:41 EDT";
   public static final String GIT_BRANCH = "vabla-climb";
-  public static final String BUILD_DATE = "2025-03-21 08:16:53 EDT";
-  public static final long BUILD_UNIX_TIME = 1742559413640L;
+  public static final String BUILD_DATE = "2025-03-21 18:53:50 EDT";
+  public static final long BUILD_UNIX_TIME = 1742597630171L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
