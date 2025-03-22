87ef0f21c459f4a81625f13de254ed207c05ceec
minhnguyenbhs <minhnguyen@mcps.org>
2025-03-22 13:52:43 -0400

    Remove no-op warmup command spam in OTF onentry

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 69;
-  public static final String GIT_SHA = "935adf47e055c0c3921a0d22ecca41638a2c09ef";
-  public static final String GIT_DATE = "2025-03-21 19:55:43 EDT";
+  public static final int GIT_REVISION = 70;
+  public static final String GIT_SHA = "7cfb9ccb850681e89be7916de3d8cd8339e9960e";
+  public static final String GIT_DATE = "2025-03-21 20:32:47 EDT";
   public static final String GIT_BRANCH = "comp/VABLA";
-  public static final String BUILD_DATE = "2025-03-21 20:31:57 EDT";
-  public static final long BUILD_UNIX_TIME = 1742603517159L;
+  public static final String BUILD_DATE = "2025-03-22 10:22:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1742653377447L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
