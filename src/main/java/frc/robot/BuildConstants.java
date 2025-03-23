1bab0bdc6774590f0cd6418ce512b4f48fd3b048
minhnguyenbhs <minhnguyen@mcps.org>
2025-03-23 09:02:40 -0400

    Tweak otf location for first score

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 71;
-  public static final String GIT_SHA = "87ef0f21c459f4a81625f13de254ed207c05ceec";
-  public static final String GIT_DATE = "2025-03-22 13:52:43 EDT";
+  public static final int GIT_REVISION = 72;
+  public static final String GIT_SHA = "19d78be04718f323137aaf96bf22f93aa2a956ca";
+  public static final String GIT_DATE = "2025-03-22 17:35:51 EDT";
   public static final String GIT_BRANCH = "comp/VABLA";
-  public static final String BUILD_DATE = "2025-03-22 17:35:10 EDT";
-  public static final long BUILD_UNIX_TIME = 1742679310414L;
+  public static final String BUILD_DATE = "2025-03-22 18:12:43 EDT";
+  public static final long BUILD_UNIX_TIME = 1742681563383L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
