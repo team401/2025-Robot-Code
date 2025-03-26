2177eb9df8a3144e46ca143ae7b798142d80ab07
minhnguyenbhs <minhnguyen@mcps.org>
2025-03-26 17:17:52 -0400

    Comp climb tweaks

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 72;
-  public static final String GIT_SHA = "19d78be04718f323137aaf96bf22f93aa2a956ca";
-  public static final String GIT_DATE = "2025-03-22 17:35:51 EDT";
+  public static final int GIT_REVISION = 73;
+  public static final String GIT_SHA = "1bab0bdc6774590f0cd6418ce512b4f48fd3b048";
+  public static final String GIT_DATE = "2025-03-23 09:02:40 EDT";
   public static final String GIT_BRANCH = "comp/VABLA";
-  public static final String BUILD_DATE = "2025-03-22 18:12:43 EDT";
-  public static final long BUILD_UNIX_TIME = 1742681563383L;
+  public static final String BUILD_DATE = "2025-03-23 14:37:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1742755077649L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
