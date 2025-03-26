325aec350b12bb8141d71192c6776f5510280626
minhnguyenbhs <minhnguyen@mcps.org>
2025-03-26 18:47:30 -0400

    use linear in tele for testing

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 75;
-  public static final String GIT_SHA = "a879c8df27b8d4eaa184a6b758a633831c7f84b1";
-  public static final String GIT_DATE = "2025-03-26 10:23:27 EDT";
+  public static final int GIT_REVISION = 76;
+  public static final String GIT_SHA = "a445792d23b1340b005ef3fa2007e9426b186414";
+  public static final String GIT_DATE = "2025-03-26 18:23:13 EDT";
   public static final String GIT_BRANCH = "linear-path-state";
-  public static final String BUILD_DATE = "2025-03-26 18:22:14 EDT";
-  public static final long BUILD_UNIX_TIME = 1743027734997L;
+  public static final String BUILD_DATE = "2025-03-26 18:42:49 EDT";
+  public static final long BUILD_UNIX_TIME = 1743028969754L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
