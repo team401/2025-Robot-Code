83795437487c223092084f2603f3ac4eb9c044ad
aidnem <99768676+aidnem@users.noreply.github.com>
2025-03-28 18:08:25 -0400

    (#206) Add constants to JSON

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 69;
-  public static final String GIT_SHA = "935adf47e055c0c3921a0d22ecca41638a2c09ef";
-  public static final String GIT_DATE = "2025-03-21 19:55:43 EDT";
+  public static final int GIT_REVISION = 70;
+  public static final String GIT_SHA = "821a9eff7172f030a35fa6a712370b1814b7b55a";
+  public static final String GIT_DATE = "2025-03-28 18:06:03 EDT";
   public static final String GIT_BRANCH = "tune-climb-better";
-  public static final String BUILD_DATE = "2025-03-28 18:05:23 EDT";
-  public static final long BUILD_UNIX_TIME = 1743199523991L;
+  public static final String BUILD_DATE = "2025-03-28 18:07:15 EDT";
+  public static final long BUILD_UNIX_TIME = 1743199635173L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
