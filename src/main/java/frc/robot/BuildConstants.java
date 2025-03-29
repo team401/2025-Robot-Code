571e46976868151d94e30475af72f423d8199ab9
aidnem <99768676+aidnem@users.noreply.github.com>
2025-03-29 17:27:12 -0400

    (#203) Pass distance to phase 1 pose to controller in phase 1 to slow down and mitigate overshoot

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 80;
-  public static final String GIT_SHA = "917e9f8c03cccbd0804271f02e9f5d63503498da";
-  public static final String GIT_DATE = "2025-03-27 20:01:36 EDT";
+  public static final int GIT_REVISION = 81;
+  public static final String GIT_SHA = "ce080c0a79cd707496cb6d2fc52acce1ea4d0fe7";
+  public static final String GIT_DATE = "2025-03-29 16:48:32 EDT";
   public static final String GIT_BRANCH = "mentor-built-linear-drive";
-  public static final String BUILD_DATE = "2025-03-29 16:23:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1743279826890L;
+  public static final String BUILD_DATE = "2025-03-29 17:15:12 EDT";
+  public static final long BUILD_UNIX_TIME = 1743282912361L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
