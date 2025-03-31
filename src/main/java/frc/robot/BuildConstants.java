7871b7fdceb74de0cfe128b569a4736b722057ad
aidnem <99768676+aidnem@users.noreply.github.com>
2025-03-31 14:20:32 -0400

    (#203) Rename reef alignment methods to be clearer andmake lineardrive use reef orbit when in phase 1

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 86;
-  public static final String GIT_SHA = "32cd723146ad1aff0320fd9535a11fd661c963e7";
-  public static final String GIT_DATE = "2025-03-31 08:59:20 EDT";
+  public static final int GIT_REVISION = 87;
+  public static final String GIT_SHA = "c69b335f92691d48694a4d0987bccb60aa32080c";
+  public static final String GIT_DATE = "2025-03-31 13:36:31 EDT";
   public static final String GIT_BRANCH = "mentor-built-linear-drive";
-  public static final String BUILD_DATE = "2025-03-31 13:35:45 EDT";
-  public static final long BUILD_UNIX_TIME = 1743442545466L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-03-31 14:18:32 EDT";
+  public static final long BUILD_UNIX_TIME = 1743445112222L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
