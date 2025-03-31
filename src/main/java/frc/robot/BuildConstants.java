bf94df276327c2cdfe0f93084311f69c149ab635
minhnguyenbhs <minhnguyen@mcps.org>
2025-03-31 19:08:46 -0400

    Foramt

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 80;
-  public static final String GIT_SHA = "917e9f8c03cccbd0804271f02e9f5d63503498da";
-  public static final String GIT_DATE = "2025-03-27 20:01:36 EDT";
-  public static final String GIT_BRANCH = "mentor-built-linear-drive";
-  public static final String BUILD_DATE = "2025-03-29 16:23:46 EDT";
-  public static final long BUILD_UNIX_TIME = 1743279826890L;
+  public static final int GIT_REVISION = 84;
+  public static final String GIT_SHA = "9d888b16a746f876e26f8b463403e35d243aafdd";
+  public static final String GIT_DATE = "2025-03-31 19:08:01 EDT";
+  public static final String GIT_BRANCH = "student-built-linear-drive";
+  public static final String BUILD_DATE = "2025-03-31 19:08:31 EDT";
+  public static final long BUILD_UNIX_TIME = 1743462511532L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
