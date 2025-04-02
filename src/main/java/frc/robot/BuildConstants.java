3a2b269d282c4c2ee6b35f7cc3028ef073d297af
minhnguyenbhs <minhnguyen@mcps.org>
2025-04-02 19:12:31 -0400

    Turn climb back on and tweak setpoint

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 81;
-  public static final String GIT_SHA = "3dec6a5746d20ff39b2dd422ccc65c3b139e00d6";
-  public static final String GIT_DATE = "2025-04-01 21:45:19 EDT";
-  public static final String GIT_BRANCH = "225-fix-linear-overshoot";
-  public static final String BUILD_DATE = "2025-04-01 21:47:52 EDT";
-  public static final long BUILD_UNIX_TIME = 1743558472426L;
+  public static final int GIT_REVISION = 76;
+  public static final String GIT_SHA = "2f2f1e23074508afe517286c263e6fb730fbf3ae";
+  public static final String GIT_DATE = "2025-04-02 08:49:38 EDT";
+  public static final String GIT_BRANCH = "229-lower-climb-ff";
+  public static final String BUILD_DATE = "2025-04-02 19:09:46 EDT";
+  public static final long BUILD_UNIX_TIME = 1743635386087L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
