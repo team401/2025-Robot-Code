7e2526232ed1670a12322134eaf2b7052b24c3ff
minhnguyenbhs <minhnguyen@mcps.org>
2025-04-02 15:15:21 -0700

    formatting?

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2025-Robot-Code-1";
+  public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 75;
-  public static final String GIT_SHA = "298d1a047bfaf25741cef8481b91f444d9aba18c";
-  public static final String GIT_DATE = "2025-04-01 17:22:40 EDT";
+  public static final int GIT_REVISION = 80;
+  public static final String GIT_SHA = "47ddb4f9278f869403669d4e05da4c1692025fb4";
+  public static final String GIT_DATE = "2025-04-02 16:58:57 EDT";
   public static final String GIT_BRANCH = "202-seed-start-position-and-rotation-in-auto";
-  public static final String BUILD_DATE = "2025-04-02 16:57:32 EDT";
-  public static final long BUILD_UNIX_TIME = 1743627452138L;
+  public static final String BUILD_DATE = "2025-04-02 18:13:49 EDT";
+  public static final long BUILD_UNIX_TIME = 1743632029779L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
