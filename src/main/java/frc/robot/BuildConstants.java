be555e07e5b0cab9bd357ae921e59d88909cec71
minhlpnguyen <93294337+minhnguyenbhs@users.noreply.github.com>
2025-04-03 16:15:40 -0700

    Cancelling OTF when close to coral station (#196)
    * ?? not quite sure if this is all you were looking for
    
    * wip, need to make the if statement cleaner + test on robot (for dcmp)
    
    * Add OTF teleop intake threshold to JSON constants and only apply threshold in teleop
    
    * Fix format because I have an overactive spacebar thumb
    
    * Addresses driver feedback by simply not OTF'ing to the intake and fixing force score button
    
    * Hopefully prevent scoring subsystem NPE in drive
    
    ---------
    
    Co-authored-by: Sojo123456 <henryolsen@mcps.org>
    Co-authored-by: aidnem <99768676+aidnem@users.noreply.github.com>

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 78;
-  public static final String GIT_SHA = "89ac08633e9c2430feef390019719ac47b439453";
-  public static final String GIT_DATE = "2025-04-02 19:23:09 EDT";
-  public static final String GIT_BRANCH = "205-algae-pop-shot";
-  public static final String BUILD_DATE = "2025-04-02 20:01:00 EDT";
-  public static final long BUILD_UNIX_TIME = 1743638460991L;
+  public static final int GIT_REVISION = 84;
+  public static final String GIT_SHA = "56a2010dce91e1f49600b96f96e76aab1e90705a";
+  public static final String GIT_DATE = "2025-04-02 20:41:04 EDT";
+  public static final String GIT_BRANCH = "close-to-intake-cancel";
+  public static final String BUILD_DATE = "2025-04-03 17:54:24 EDT";
+  public static final long BUILD_UNIX_TIME = 1743717264571L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -45,6 +45,7 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.TestModeManager;
 import frc.robot.constants.JsonConstants;
 import frc.robot.constants.ModeConstants;
+import frc.robot.constants.ModeConstants.Mode;
 import frc.robot.subsystems.drive.states.IdleState;
 import frc.robot.subsystems.drive.states.JoystickDrive;
 import frc.robot.subsystems.drive.states.LinearDriveState;
@@ -397,12 +398,23 @@ public class Drive implements DriveTemplate {
   @Override
   public void periodic() {
     // Manually cancel go to intake if we have a gamepiece
-    if (ScoringSubsystem.getInstance() != null) {
-      if (goToIntake && ScoringSubsystem.getInstance().isCoralDetected()) {
-        setGoToIntake(false);
-      } else if (goToIntake && ScoringSubsystem.getInstance().isAlgaeDetected()) {
-        setGoToIntake(false);
-      }
+    if (goToIntake
+        && (ScoringSubsystem.getInstance() == null
+            || ScoringSubsystem.getInstance().isCoralDetected())) {
+      setGoToIntake(false);
+    } else if (goToIntake
+        && (ScoringSubsystem.getInstance() == null
+            || ScoringSubsystem.getInstance().isAlgaeDetected())) {
+      setGoToIntake(false);
+    } else if (goToIntake
+        && ModeConstants.currentMode == Mode.REAL
+        && this.getPose()
+                .getTranslation()
+                .getDistance(OTFState.findOTFPoseFromDesiredLocation(this).getTranslation())
+            < JsonConstants.drivetrainConstants.teleopOTFIntakeThresholdMeters
+        && DriverStation.isTeleop()) {
+      // setGoToIntake(false);
+      this.fireTrigger(DriveTrigger.ManualJoysticks);
     }
 
     odometryLock.lock(); // Prevents odometry updates while reading data
