7a92960e7c0899b5cf68fb3ec15043766491885b
minhnguyenbhs <minhnguyen@mcps.org>
2025-04-23 19:57:01 -0400

    tested in shop + added demo mode

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 82;
-  public static final String GIT_SHA = "dade2d89334b2a98824bba01a60f37f602c4269f";
-  public static final String GIT_DATE = "2025-04-23 18:34:33 EDT";
+  public static final int GIT_REVISION = 84;
+  public static final String GIT_SHA = "2f02957a9a19e386fca4b3f39b836bf40888dc15";
+  public static final String GIT_DATE = "2025-04-23 19:14:28 EDT";
   public static final String GIT_BRANCH = "233-add-support-for-gamepad-based-driving";
-  public static final String BUILD_DATE = "2025-04-23 18:38:01 EDT";
-  public static final long BUILD_UNIX_TIME = 1745447881356L;
+  public static final String BUILD_DATE = "2025-04-23 19:51:13 EDT";
+  public static final long BUILD_UNIX_TIME = 1745452273112L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
