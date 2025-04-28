05f2895617183591d7692b4134d9e0d41f9fbd2f
minhnguyenbhs <minhnguyen@mcps.org>
2025-04-28 19:23:23 -0400

    (Retune) Update right camera crosstrack offset

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,7 +5,6 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code-1";
   public static final String VERSION = "unspecified";
-
   public static final int GIT_REVISION = 98;
   public static final String GIT_SHA = "1ad7379e12ee7f0a6077777e6b07841b55e236c3";
   public static final String GIT_DATE = "2025-04-28 18:35:45 EDT";
