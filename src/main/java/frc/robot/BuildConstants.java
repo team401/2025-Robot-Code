177731c39bc07baedea123ae5b6fbd5916da1b8d
minhnguyenbhs <minhnguyen@mcps.org>
2025-05-12 18:35:40 -0400

    Increase rotation speed and acceleration in linear drive

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 84;
-  public static final String GIT_SHA = "12b91edda34b967fdb9d13bac260c911fa2b8f45";
-  public static final String GIT_DATE = "2025-04-28 20:00:44 EDT";
+  public static final int GIT_REVISION = 85;
+  public static final String GIT_SHA = "7268d88a541e204ca830e1c76469d75841ec800d";
+  public static final String GIT_DATE = "2025-05-05 20:00:28 EDT";
   public static final String GIT_BRANCH = "april-auto-retune";
-  public static final String BUILD_DATE = "2025-05-05 19:56:15 EDT";
-  public static final long BUILD_UNIX_TIME = 1746489375480L;
+  public static final String BUILD_DATE = "2025-05-12 18:23:24 EDT";
+  public static final long BUILD_UNIX_TIME = 1747088604348L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
