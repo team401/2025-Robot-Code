bf849482b207e99dbef557e890bb630fc8c8e557
aidnem <99768676+aidnem@users.noreply.github.com>
2025-09-16 09:37:25 -0400

    Add README (#251)
    * (#248) Add instructions on how to clone the repository and set up 3D model in sim
    
    * (#248) Omit unnecessary headings from TOC and fix typo
    
    * (#248) WIP - Start adding project features section to README
    
    * (#248) Elaborate on strategy manager in README.
    
    After this I'm switching back to Sim usage instructions until those are
    ready for use.
    
    * (#248) WIP - Begin describing how to drive the robot and score in auto & teleop in sim
    
    * (#248) WIP - Add instructions to drive the robot to README
    
    * (#248) Add instructions on sim usage for autos
    
    * (#248) Format
    
    * (#248) Add to configure IP in Elastic
    
    * (#248) Add Using SnakeScreen to README
    
    * (#248) Begin describing project structure in README
    
    * (#248) Improve detail in componentPositions instructions and describe environments
    
    * (#248) Add provision for bad instructions to instructions
    
    * (#248) Add demo to environment descriptions because I previously forgot it

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 85;
-  public static final String GIT_SHA = "7268d88a541e204ca830e1c76469d75841ec800d";
-  public static final String GIT_DATE = "2025-05-05 20:00:28 EDT";
-  public static final String GIT_BRANCH = "april-auto-retune";
-  public static final String BUILD_DATE = "2025-05-12 18:23:24 EDT";
-  public static final long BUILD_UNIX_TIME = 1747088604348L;
+  public static final int GIT_REVISION = 94;
+  public static final String GIT_SHA = "8f0396d74d2a8c697941aada0550766844303745";
+  public static final String GIT_DATE = "2025-08-22 17:46:12 EDT";
+  public static final String GIT_BRANCH = "248-add-readme";
+  public static final String BUILD_DATE = "2025-08-23 21:35:05 EDT";
+  public static final long BUILD_UNIX_TIME = 1755999305676L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
