32a84517bfc61b4bc1aec54723426579bb78f49e
minhnguyenbhs <minhnguyen@mcps.org>
2025-10-04 09:57:04 -0400

    blue ridge brawl field cal

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 83;
-  public static final String GIT_SHA = "78548d16519e17e819c804a9da722a5ba1254a08";
-  public static final String GIT_DATE = "2025-05-12 18:41:14 EDT";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-09-24 19:25:52 EDT";
-  public static final long BUILD_UNIX_TIME = 1758756352928L;
+  public static final int GIT_REVISION = 84;
+  public static final String GIT_SHA = "d70c73d2628fc0af4eb13db3f9ecc7d61461dd2b";
+  public static final String GIT_DATE = "2025-09-24 19:27:13 EDT";
+  public static final String GIT_BRANCH = "september-tuneup";
+  public static final String BUILD_DATE = "2025-10-04 08:43:35 EDT";
+  public static final long BUILD_UNIX_TIME = 1759581815985L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
