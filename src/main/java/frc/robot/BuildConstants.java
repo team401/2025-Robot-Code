d7409a65566742ead16543e22cc4d4ec0395c7fe
minhnguyenbhs <minhnguyen@mcps.org>
2025-10-08 19:59:16 -0400

    Try inverting the drive rotation axis. This was BACKWARD before I inverted it.

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 84;
-  public static final String GIT_SHA = "d70c73d2628fc0af4eb13db3f9ecc7d61461dd2b";
-  public static final String GIT_DATE = "2025-09-24 19:27:13 EDT";
+  public static final int GIT_REVISION = 85;
+  public static final String GIT_SHA = "32a84517bfc61b4bc1aec54723426579bb78f49e";
+  public static final String GIT_DATE = "2025-10-04 09:57:04 EDT";
   public static final String GIT_BRANCH = "september-tuneup";
-  public static final String BUILD_DATE = "2025-10-04 08:43:35 EDT";
-  public static final long BUILD_UNIX_TIME = 1759581815985L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-10-08 19:56:35 EDT";
+  public static final long BUILD_UNIX_TIME = 1759967795060L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
