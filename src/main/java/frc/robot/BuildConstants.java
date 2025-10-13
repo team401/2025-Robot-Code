3e5c4028af16d92f24e435e72275083cb638827b
team401 <401frc@gmail.com>
2025-10-13 19:56:27 -0400

    (#252) Untested - Update left lineup gains based on shop testing

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 85;
-  public static final String GIT_SHA = "6a7f7502ff530be9c2e81260a9b92c8d1310d710";
-  public static final String GIT_DATE = "2025-10-10 13:49:36 EDT";
+  public static final int GIT_REVISION = 87;
+  public static final String GIT_SHA = "701d91a281c5147d4427b2142b25c52856767b3c";
+  public static final String GIT_DATE = "2025-10-13 18:41:28 EDT";
   public static final String GIT_BRANCH = "252-separate-lineup-offsets";
-  public static final String BUILD_DATE = "2025-10-11 13:08:34 EDT";
-  public static final long BUILD_UNIX_TIME = 1760202514694L;
+  public static final String BUILD_DATE = "2025-10-13 18:46:12 EDT";
+  public static final long BUILD_UNIX_TIME = 1760395572545L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
