2374960507b18c0c1fcbdd5c717504f84793e7d0
aidnem <99768676+aidnem@users.noreply.github.com>
2025-10-24 15:22:53 -0400

    (#252) Update non-comp JSON environments with new separate alongtrack values

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Robot-Code";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 89;
-  public static final String GIT_SHA = "69fe6668bc357bc838edcbd85dd68452dfd47f76";
-  public static final String GIT_DATE = "2025-10-22 19:33:06 EDT";
+  public static final int GIT_REVISION = 90;
+  public static final String GIT_SHA = "13ec3b6f046838dd98b05d9f82f2362eb77383c4";
+  public static final String GIT_DATE = "2025-10-24 15:19:25 EDT";
   public static final String GIT_BRANCH = "252-separate-lineup-offsets";
-  public static final String BUILD_DATE = "2025-10-24 15:18:38 EDT";
-  public static final long BUILD_UNIX_TIME = 1761333518349L;
+  public static final String BUILD_DATE = "2025-10-24 15:21:57 EDT";
+  public static final long BUILD_UNIX_TIME = 1761333717458L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
