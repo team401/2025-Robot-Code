4d8b048cee6cf33db0c9b50fe69e28043d2343e9
aidnem <99768676+aidnem@users.noreply.github.com>
2025-11-15 02:46:27 +0000

    Fix advantagescope run script. It can now launch from VSCode :D

--- .devcontainer/buildscripts/runscripts/AdvantageScope.sh
+++ .devcontainer/buildscripts/runscripts/AdvantageScope.sh
@@ -3,5 +3,9 @@
 SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 SCRIPT_NAME="$(basename "$(realpath "$0")")"
 SCRIPT_BASE="$(basename -s .sh "$SCRIPT_NAME")"
+OS_NAME="$(uname -s)"
+AS_PATH="/usr/bin"
 
-/usr/bin/advantagescope --no-sandbox
+unset ELECTRON_RUN_AS_NODE
+
+exec "$AS_PATH/advantagescope" "--no-sandbox"
